bd9d534e3c1d0d285e2074a14838daaae55cc1b7
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-15 22:30:06 +0100

    chore: shorten pipe implementation

--- src/dsl/fp/pipe.js
+++ src/dsl/fp/pipe.js
@@ -1,10 +1,5 @@
 module.exports = (f1, ...fs) => a => {
   let res = f1(a)
-
-  for (let i = 0; i < fs.length; i++) {
-    const f = fs[i]
-    res = f(res)
-  }
-
+  for (let i = 0; i < fs.length; i++) res = fs[i](res)
   return res
 }
