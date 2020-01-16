678c7bb1bda4d6e3f268fbefdfeb2212dbfa1c3a
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-16 22:04:46 +0100

    chore: rename f1 to just f

--- src/dsl/fp/pipe.js
+++ src/dsl/fp/pipe.js
@@ -1,5 +1,5 @@
-module.exports = (f1, ...fs) => a => {
-  let res = f1(a)
+module.exports = (f, ...fs) => a => {
+  let res = f(a)
   for (let i = 0; i < fs.length; i++) res = fs[i](res)
   return res
 }
