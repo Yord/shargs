0872741182579f9dc945bc538b5cdb4528e89de5
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-17 20:55:23 +0100

    test: add a test for option's error message if no arg is given

--- src/dsl/fp/option.test.js
+++ src/dsl/fp/option.test.js
@@ -39,3 +39,43 @@ test('option transforms options DSL into arguments DSL', () => {
     })
   )
 })
+
+test('option fails if no arg is given', () => {
+  const optionsArguments = array(base64(), 1, 20).chain(args =>
+    anything().chain(types =>
+      anything().chain(only =>
+        anything().chain(desc =>
+          anything().map(opts => {
+            const options = {
+              types: typeof types !== 'undefined' ? types : null,
+              only:  typeof only  !== 'undefined' ? only  : null,
+              desc:  typeof desc  !== 'undefined' ? desc  : '',
+              opts:  typeof opts  !== 'undefined' ? opts  : null
+            }
+            return {
+              options: {...options, args},
+              _arguments: {
+                errs: [{
+                  code: 'No argument provided in option',
+                  msg:  "Please provide an arg key (e.g. {arg: 'foo'})",
+                  info: {options: {...options, args}}
+                }],
+                args: {}
+              }
+            }
+          })
+        )
+      )
+    )
+  )
+
+  assert(
+    property(optionsArguments, ({options, _arguments}) => {
+      expect(
+        option(options)
+      ).toStrictEqual(
+        _arguments
+      )
+    })
+  )
+})
