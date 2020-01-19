cf030f8ded5aa833353ffdd64fc324e094a68c04
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-19 22:35:47 +0100

    feat: combine arrays instead of objects

--- src/dsl/fp/combine.js
+++ src/dsl/fp/combine.js
@@ -10,10 +10,32 @@ module.exports = (...options) => {
     } else {
       const keys = Object.keys(args)
 
-      for (let i = 0; i < keys.length; i++) {
-        const key   = keys[i]
-        const value = args[key]
-        args2[key]  = value
+      for (let j = 0; j < keys.length; j++) {
+        const key  = keys[j]
+        const list = args[key]
+        if (typeof args2[key] === 'undefined') {
+          if (typeof list !== 'undefined' && list !== null && list.length > 0) {
+            args2[key] = list
+          } else {
+            args2[key] = []
+            // ADD ERROR!
+            errs2.push({msg: 'FOOOOO #1', info: {list}})
+          }
+        } else {
+          const types = args2[key][0].types || []
+          for (let k = 0; k < list.length; k++) {
+            const elem = list[k]
+            if (typeof elem.types === 'undefined') {
+              // ADD ERROR!
+              errs2.push({msg: 'FOOOOO #2', info: {elem}})
+            } else if (elem.types === types || elem.types.length === types.length) {
+              args2[key].push(elem)
+            } else {
+              // ADD ERROR!
+              errs2.push({msg: 'FOOOOO #3', info: {elem}})
+            }
+          }
+        }
       }
     }
   }
