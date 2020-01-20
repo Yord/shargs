8b9288b5969d72eff5da0d04faedea78e3214090
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-20 19:43:39 +0100

    chore: rename postprocess to preprocess

--- src/parser/parseArgs.js
+++ src/parser/parseArgs.js
@@ -1,4 +1,4 @@
-module.exports = postprocess => ({args = []} = {}) => ({errs = [], argv = []} = {}) => {
+module.exports = preprocess => ({args = []} = {}) => ({errs = [], argv = []} = {}) => {
   let errs2   = []
   const argv2 = []
 
@@ -32,7 +32,7 @@ module.exports = postprocess => ({args = []} = {}) => ({errs = [], argv = []} = 
           arr = argv.slice(at + 1, at + types.length + 1)
         }
 
-        const res = postprocess(option)({errs, argv: arr})
+        const res = preprocess(option)({errs, argv: arr})
 
         errs2 = errs2.concat(res.errs)
         argv2.push([key, res.argv, option.types, opts])
