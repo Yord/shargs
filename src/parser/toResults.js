545f12c21dc2be6c8ca8c5950e7a77c21a0343b6
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-25 21:17:06 +0100

    chore: rename output field from argv to args

--- src/parser/toResults.js
+++ src/parser/toResults.js
@@ -1,24 +1,24 @@
 module.exports = parser => ({errs = [], argv = []} = {}) => {
   let errs2   = []
-  const argv2 = {_: []}
+  const args = {_: []}
 
   for (let i = 0; i < argv.length; i++) {
     const {key, values, types, opts} = argv[i]
 
     if (typeof types === 'undefined') {
-      if (key !== '--') argv2['_'] = argv2['_'].concat(values)
+      if (key !== '--') args['_'] = args['_'].concat(values)
     } else if (types === null) {
       const parse    = parser(opts || [])
-      const {errs: errs3, argv} = parse({errs: [], argv: values})
+      const {errs: errs3, args: args2} = parse({errs: [], argv: values})
 
       errs2      = errs3
-      argv2[key] = Object.assign({}, argv2[key], argv)
+      args[key] = Object.assign({}, args[key], args2)
     } else if (types.length === 0) {
-      argv2[key] = typeof argv2[key] === 'undefined' ? true : argv2[key] < 2 ? 2 : argv2[key] + 1
+      args[key] = typeof args[key] === 'undefined' ? true : args[key] < 2 ? 2 : args[key] + 1
     } else {
-      argv2[key] = types.length === 1 ? values[0] : values
+      args[key] = types.length === 1 ? values[0] : values
     }
   }
 
-  return {errs: errs.concat(errs2), argv: argv2}
+  return {errs: errs.concat(errs2), args}
 }
