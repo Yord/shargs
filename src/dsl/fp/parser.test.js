f264e878edb243cc98fade8baa3149d44ec73675
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-25 14:41:52 +0100

    test: add a parser test that verifies all errors are passed on

---
+++ src/dsl/fp/parser.test.js
@@ -0,0 +1,34 @@
+const {anything, array, assert, base64, constant, integer, oneof, property} = require('fast-check')
+const parser = require('./parser')
+
+test('parser concats errors', () => {
+  const id = a => a
+
+  const initInputResult = (
+    oneof(constant(undefined), array(anything(), 1, 5).map(errs => ({errs})))
+    .chain(init =>
+      oneof(constant(undefined), array(anything(), 1, 5).map(errs => ({errs})))
+      .map(input => ({
+        init,
+        input,
+        result: {
+          errs: [
+            ...((init  || {}).errs || []),
+            ...((input || {}).errs || [])
+          ],
+          argv: []
+        }
+      }))
+    )
+  )
+
+  assert(
+    property(initInputResult, ({init, input, result}) =>
+      expect(
+        parser(id)(init)(input)
+      ).toStrictEqual(
+        result
+      )
+    )
+  )
+})
