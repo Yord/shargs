d03ddd2ac8247a1181488e6ccdef4fa33f51114d
Philipp Wille <philipp.y.wille@gmail.com>
2020-02-08 21:35:07 +0100

    refactor: move notes to its own module

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ const table  = require('./src/help/layout/table')
 const text   = require('./src/help/layout/text')
 const texts  = require('./src/help/layout/texts')
 const usage  = require('./src/help/usage')
+const notes  = require('./src/help/usage/notes')
 
 
 
@@ -762,10 +763,6 @@ console.log('exD1  === exD2',  exD1  === exD2)
 
 
 // The following functions automatically deal with strings that contain opts
-
-function notes (strings = [], id = undefined) {
-  return () => texts(strings, id)
-}
 
 function note (string = '', id = undefined) {
   return () => text(string, id)
