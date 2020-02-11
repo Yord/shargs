4aa1e2ed7688f000355fa666f3b624aeae151fb8
Philipp Wille <philipp.y.wille@gmail.com>
2020-02-11 21:12:15 +0100

    chore: set default filter to the tautology

--- src/help/usage/optsDefs.js
+++ src/help/usage/optsDefs.js
@@ -1,12 +1,12 @@
 const defs = require('../layout/defs')
 
-module.exports = (filter = ({types}) => typeof types !== 'undefined' && types !== null, id = undefined) => (
+module.exports = (filter = () => true, id = undefined) => (
   (opts = []) => {
     const items = (
       opts
       .filter(filter)
-      .map(opt => ({types} = opt, types !== null && types.length === 0 ? {...opt, types: ['flag']} : opt))
-      .map(({args, desc, types}) => ({
+      .map(opt => ({types} = opt, Array.isArray(types) && types.length === 0 ? {...opt, types: ['flag']} : opt))
+      .map(({args = [], desc = '', types}) => ({
         title: args.join(', ') + ' [' + types.join(', ') + ']',
         desc
       }))
