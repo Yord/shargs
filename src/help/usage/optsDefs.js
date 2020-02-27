d365c49ef0b78481b3ded397661015b2f4c2a2bc
Philipp Wille <philipp.y.wille@gmail.com>
2020-02-27 16:49:36 +0100

    refactor: change optsDefs function by pulling out id into its own parameter

--- src/help/usage/optsDefs.js
+++ src/help/usage/optsDefs.js
@@ -1,17 +1,22 @@
 const {defsFrom} = require('../layout/defs')
 
-module.exports = (filter = () => true, id = 'defs') => (
-  (opts = []) => {
-    const items = (
-      opts
-      .filter(filter)
-      .map(opt => ({types} = opt, Array.isArray(types) && types.length === 0 ? {...opt, types: ['flag']} : opt))
-      .map(({args = [], desc = '', types}) => ({
-        title: args.join(', ') + ' [' + types.join(', ') + ']',
-        desc
-      }))
-    )
+const optsDefsFrom = id => (filter = () => true) => (opts = []) => {
+  const items = (
+    opts
+    .filter(filter)
+    .map(opt => ({types} = opt, Array.isArray(types) && types.length === 0 ? {...opt, types: ['flag']} : opt))
+    .map(({args = [], desc = '', types}) => ({
+      title: args.join(', ') + ' [' + types.join(', ') + ']',
+      desc
+    }))
+  )
 
-    return defsFrom(id)(items)
-  }
-)
+  return defsFrom(id)(items)
+}
+
+const optsDefs = optsDefsFrom('defs')
+
+module.exports = {
+  optsDefs,
+  optsDefsFrom
+}
