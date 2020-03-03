5a1c1408215aee0469d43ec977794ff8f8d83fb7
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-03 21:22:31 +0100

    refactor: move out onlyCommands into its own module

--- src/help/usage/decorators/usageDecorators.js
+++ src/help/usage/decorators/usageDecorators.js
@@ -1,9 +1,4 @@
-const optsFilter = require('./optsFilter')
 const optsMap = require('./optsMap')
-
-const onlyCommands = optsFilter(
-  ({types}) => typeof types === 'undefined' || types === null
-)
 
 const onlyFirstArg = optsMap(
   opt => ({...opt, args: (opt.args || []).slice(0, 1)})
