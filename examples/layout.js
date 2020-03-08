d194a2aee447e9fa4891fe9878c7b5822b0d31ad
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-08 12:41:27 +0100

    chore: add examples for the layout and layoutMap combinators

--- examples/layout.js
+++ examples/layout.js
@@ -1,14 +1,15 @@
-const layout  = require('../src/help/layout/combinators/layout')
+const layout           = require('../src/help/layout/combinators/layout')
+const layoutMap        = require('../src/help/layout/combinators/layoutMap')
 
-const {br}    = require('../src/help/layout/br')
-const {brs}   = require('../src/help/layout/brs')
-const {cols}  = require('../src/help/layout/cols')
-const {defs}  = require('../src/help/layout/defs')
-const {line}  = require('../src/help/layout/line')
-const {lines} = require('../src/help/layout/lines')
-const {table} = require('../src/help/layout/table')
-const {text}  = require('../src/help/layout/text')
-const {texts} = require('../src/help/layout/texts')
+const {br}             = require('../src/help/layout/br')
+const {brs}            = require('../src/help/layout/brs')
+const {cols}           = require('../src/help/layout/cols')
+const {defs}           = require('../src/help/layout/defs')
+const {line}           = require('../src/help/layout/line')
+const {lines}          = require('../src/help/layout/lines')
+const {table}          = require('../src/help/layout/table')
+const {text, textFrom} = require('../src/help/layout/text')
+const {texts}          = require('../src/help/layout/texts')
 
 ;(function () {
   const style = {
@@ -134,6 +135,46 @@ const {texts} = require('../src/help/layout/texts')
     'Deep Thought was created to come up with the Answer.',
     'To The Ultimate Question of Life, the Universe, and Everything.'
   ])(style)
+
+  console.log(res)
+}())
+
+;(function () {
+  const style = {
+    line: {width: 40}
+  }
+
+  const res = layout([
+    line('First line'),
+    line('Last line')
+  ])(style)
+
+  console.log(res)
+}())
+
+;(function () {
+  const style = {
+    line: {width: 40},
+    desc: {padStart: 3, width: 37}
+  }
+
+  const itemsList = [
+    [
+      '-h, --help',
+      'Prints the help.'
+    ],
+    [
+      '-v, --version',
+      'Prints the version.'
+    ]
+  ]
+
+  const f = ([title, desc]) => [
+    text(title),
+    textFrom('desc')(desc)
+  ]
+
+  const res = layoutMap(f)(itemsList)(style)
 
   console.log(res)
 }())
