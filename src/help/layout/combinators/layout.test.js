8d4ab4f0775624a594a9a4fd6bfc492e8390c8e7
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-15 20:58:21 +0100

    test: test if layout uses default style if style is undefined

--- src/help/layout/combinators/layout.test.js
+++ src/help/layout/combinators/layout.test.js
@@ -1,4 +1,5 @@
 const layout = require('./layout')
+const {line} = require('../line')
 
 test('layout returns the empty string if no functions are defined', () => {
   const style = {
@@ -8,6 +9,16 @@ test('layout returns the empty string if no functions are defined', () => {
   const res = layout()(style)
 
   const txt = ''
+
+  expect(res).toStrictEqual(txt)
+})
+
+test('layout uses default style if style is undefined', () => {
+  const res = layout([
+    line('layout uses default style if style is undefined')
+  ])()
+
+  const txt = 'layout uses default style if style is undefined                                 \n'
 
   expect(res).toStrictEqual(txt)
 })
