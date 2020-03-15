d306e929319773e4e6fe3cbafbcee874a30c7fc2
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-15 16:01:49 +0100

    test: test if brsFrom correctly passes on id

--- src/help/layout/brs.test.js
+++ src/help/layout/brs.test.js
@@ -1,4 +1,4 @@
-const {brs} = require('./brs')
+const {brs, brsFrom} = require('./brs')
 
 test('brs generates expected string', () => {
   const style = {
@@ -21,6 +21,21 @@ test('brs with default length generates expected string', () => {
   const res = brs()(style)
 
   const txt = '                                        \n'
+
+  expect(res).toStrictEqual(txt)
+})
+
+test('brsFrom correctly passes on id', () => {
+  const id = 'test'
+  
+  const style = {
+    [id]: {width: 40}
+  }
+
+  const res = brsFrom(id)(2)(style)
+
+  const txt = '                                        \n' +
+              '                                        \n'
 
   expect(res).toStrictEqual(txt)
 })
