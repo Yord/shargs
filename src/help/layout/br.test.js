225001d35c0ea6dc2d17e6ff74f44a997dda8627
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-15 15:51:27 +0100

    chore: refine wording

--- src/help/layout/br.test.js
+++ src/help/layout/br.test.js
@@ -1,13 +1,13 @@
 const {br} = require('./br')
 
-test('br layouts as expected', () => {
+test('br generates expected string', () => {
   const style = {
     line: {width: 40}
   }
 
   const res = br(style)
 
-  const foo = '                                        \n'
+  const txt = '                                        \n'
 
-  expect(res).toStrictEqual(foo)
+  expect(res).toStrictEqual(txt)
 })
