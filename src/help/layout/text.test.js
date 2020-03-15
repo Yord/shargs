ac80d9c48dcb580f5995bded96e01793edf0b51d
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-15 18:35:31 +0100

    test: test if text generates expected string

---
+++ src/help/layout/text.test.js
@@ -0,0 +1,14 @@
+const {text} = require('./text')
+
+test('text generates expected string', () => {
+  const style = {
+    line: {width: 40}
+  }
+  
+  const res = text('Deep Thought was created to come up with the Answer.')(style)
+
+  const txt = 'Deep Thought was created to come up with\n' +
+              'the Answer.                             \n'
+
+  expect(res).toStrictEqual(txt)
+})
