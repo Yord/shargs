4e71d153a9807411591a90bee8d32cc9840f519d
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-15 18:36:51 +0100

    test: test if text retains more than one consecutive whitespace even after line breaks

--- src/help/layout/text.test.js
+++ src/help/layout/text.test.js
@@ -12,3 +12,16 @@ test('text generates expected string', () => {
 
   expect(res).toStrictEqual(txt)
 })
+
+test('text retains more than one consecutive whitespace even after line breaks', () => {
+  const style = {
+    line: {width: 40}
+  }
+  
+  const res = text('Deep Thought was created to come up with  the Answer.')(style)
+
+  const txt = 'Deep Thought was created to come up with\n' +
+              '  the Answer.                           \n'
+
+  expect(res).toStrictEqual(txt)
+})
