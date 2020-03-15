b77636c9a11f4658494cddf2f9364808b442ee2e
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-15 16:46:58 +0100

    test: test if defsFrom correctly passes on second id

--- src/help/layout/defs.test.js
+++ src/help/layout/defs.test.js
@@ -56,3 +56,33 @@ test('defsFrom correctly passes on first id', () => {
 
   expect(res).toStrictEqual(txt)
 })
+
+test('defsFrom correctly passes on second id', () => {
+  const id = 'test'
+  
+  const style = {
+    line: {width: 40},
+    test: {padStart: 4, width: 26},
+    desc: {padStart: 4, width: 36}
+  }
+  
+  const res = defsFrom('line', 'test')([
+    [
+      '-h, --help',
+      'Prints the help.'
+    ],
+    [
+      '-v, --version',
+      'Prints the version.'
+    ]
+  ])(style)
+
+  const txt = '-h, --help                              \n' +
+              '    Prints the help.          \n' +
+              '                                        \n' +
+              '-v, --version                           \n' +
+              '    Prints the version.       \n' +
+              '                                        \n'
+
+  expect(res).toStrictEqual(txt)
+})
