bc54c3cdb0776d1b8e2a01458d6fe234a812e119
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-15 18:06:09 +0100

    test: test if table drops all input that have no cols in style

--- src/help/layout/table.test.js
+++ src/help/layout/table.test.js
@@ -25,3 +25,28 @@ test('table generates expected string', () => {
 
   expect(res).toStrictEqual(txt)
 })
+
+test('table drops all input that have no cols in style', () => {
+  const style = {
+    cols: [
+      {width: 12}
+    ]
+  }
+  
+  const res = table([
+    [
+      '-h, --help',
+      'Prints the help.'
+    ],
+    [
+      '-v, --version',
+      'Prints the version.'
+    ]
+  ])(style)
+
+  const txt = '-h, --help  \n' +
+              '-v,         \n' +
+              '--version   \n'
+
+  expect(res).toStrictEqual(txt)
+})
