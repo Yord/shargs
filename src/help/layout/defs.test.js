bbd4a16f38e97840437a5a0adf37db50d21f408a
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-15 16:45:13 +0100

    test: test if defsFrom correctly passes on first id

--- src/help/layout/defs.test.js
+++ src/help/layout/defs.test.js
@@ -1,4 +1,4 @@
-const {defs} = require('./defs')
+const {defs, defsFrom} = require('./defs')
 
 test('defs generates expected string', () => {
   const style = {
@@ -21,6 +21,36 @@ test('defs generates expected string', () => {
               '    Prints the help.                    \n' +
               '                                        \n' +
               '-v, --version                           \n' +
+              '    Prints the version.                 \n' +
+              '                                        \n'
+
+  expect(res).toStrictEqual(txt)
+})
+
+test('defsFrom correctly passes on first id', () => {
+  const id = 'test'
+  
+  const style = {
+    line: {width: 40},
+    test: {width: 20},
+    desc: {padStart: 4, width: 36}
+  }
+  
+  const res = defsFrom(id, 'desc')([
+    [
+      '-h, --help',
+      'Prints the help.'
+    ],
+    [
+      '-v, --version',
+      'Prints the version.'
+    ]
+  ])(style)
+
+  const txt = '-h, --help          \n' +
+              '    Prints the help.                    \n' +
+              '                                        \n' +
+              '-v, --version       \n' +
               '    Prints the version.                 \n' +
               '                                        \n'
 
