f19d8f253546e335e943cd9ee97e938fb42a70f6
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-16 19:02:59 +0100

    test: test if optsDefs generates expected string

---
+++ src/help/usage/optsDefs.test.js
@@ -0,0 +1,29 @@
+const {optsDefs} = require('./optsDefs')
+const {command, flag, number} = require('../../options')
+
+test('optsDefs generates expected string', () => {
+  const opts = [
+    number('answer', ['-a', '--answer'], {desc: 'The answer.'}),
+    command('help', ['-h', '--help'], {desc: 'Prints help.'}),
+    flag('version', ['--version'], {desc: 'Prints version.'})
+  ]
+
+  const style = {
+    line: {width: 40},
+    desc: {padStart: 4, width: 36}
+  }
+  
+  const res = optsDefs(opts)(style)
+
+  const txt = '-a, --answer [number]                   \n' +
+              '    The answer.                         \n' +
+              '                                        \n' +
+              '-h, --help                              \n' +
+              '    Prints help.                        \n' +
+              '                                        \n' +
+              '--version [flag]                        \n' +
+              '    Prints version.                     \n' +
+              '                                        \n'
+
+  expect(res).toStrictEqual(txt)
+})
