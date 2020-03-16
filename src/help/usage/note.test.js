e92130e26174e6b4d92a1e7eb426962b857efa8b
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-16 18:27:13 +0100

    test: test if note generates expected string

---
+++ src/help/usage/note.test.js
@@ -0,0 +1,18 @@
+const {note} = require('./note')
+
+test('note generates expected string', () => {
+  const opts = []
+
+  const style = {
+    line: {width: 40}
+  }
+
+  const res = note(
+    'Deep Thought was created to come up with the Answer.'
+  )(opts)(style)
+
+  const txt = 'Deep Thought was created to come up with\n' +
+              'the Answer.                             \n'
+
+  expect(res).toStrictEqual(txt)
+})
