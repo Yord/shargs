d5b74dd8aeb4b74ac072e29399d9824e581319dd
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-16 20:36:25 +0100

    test: test if optsList keeps whitespaces if there are several

--- src/help/usage/optsList.test.js
+++ src/help/usage/optsList.test.js
@@ -24,3 +24,27 @@ test('optsList generates expected string', () => {
 
   expect(res).toStrictEqual(txt)
 })
+
+test('optsList keeps whitespaces if there are several', () => {
+  const opts = [
+    number('answer', ['-a', '--answer'], {desc: 'The  answer.'}),
+    command('help', ['-h', '--help'], {desc: 'Prints help.'}),
+    flag('version', ['--version'], {desc: 'Prints version.'})
+  ]
+
+  const style = {
+    cols: [
+      {width: 10, padEnd: 2},
+      {width: 28}
+    ]
+  }
+  
+  const res = optsList(opts)(style)
+
+  const txt = '-a,         The  answer. [number]       \n' +
+              '--answer                                \n' +
+              '-h, --help  Prints help.                \n' +
+              '--version   Prints version. [flag]      \n'
+
+  expect(res).toStrictEqual(txt)
+})
