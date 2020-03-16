4e0bfefb3b13693c679d55a5c23bfd5b036ee279
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-16 18:43:11 +0100

    test: test if notesFrom correctly passes on id

--- src/help/usage/notes.test.js
+++ src/help/usage/notes.test.js
@@ -1,4 +1,4 @@
-const {notes} = require('./notes')
+const {notes, notesFrom} = require('./notes')
 
 test('notes generates expected string', () => {
   const opts = []
@@ -112,6 +112,28 @@ test('notes uses default style if style has no line attribute', () => {
 
   const txt = 'Deep Thought was created to come up with the Answer.                            \n' +
               'To The Ultimate Question of Life, the Universe, and Everything.                 \n'
+
+  expect(res).toStrictEqual(txt)
+})
+
+test('notesFrom correctly passes on id', () => {
+  const id = 'test'
+
+  const opts = []
+  
+  const style = {
+    [id]: {width: 40}
+  }
+
+  const res = notesFrom(id)([
+    'Deep Thought was created to come up with the Answer.',
+    'To The Ultimate Question of Life, the Universe, and Everything.'
+  ])(opts)(style)
+
+  const txt = 'Deep Thought was created to come up with\n' +
+              'the Answer.                             \n' +
+              'To The Ultimate Question of Life, the   \n' +
+              'Universe, and Everything.               \n'
 
   expect(res).toStrictEqual(txt)
 })
