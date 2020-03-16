62f347c91a71b997703ec127911c213b477fe202
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-16 22:08:36 +0100

    test: test if synopsis also works with just the first input

--- src/help/usage/synopsis.test.js
+++ src/help/usage/synopsis.test.js
@@ -1,4 +1,4 @@
-const {synopsis} = require('./synopsis')
+const {synopsis, synopsisFrom} = require('./synopsis')
 const {command, flag, number} = require('../../options')
 
 test('synopsis generates expected string', () => {
@@ -16,6 +16,25 @@ test('synopsis generates expected string', () => {
 
   const txt = 'deepThought [-a|--answer] [-h|--help]   \n' +
               '            [--version] <QUESTION>      \n'
+
+  expect(res).toStrictEqual(txt)
+})
+
+test('synopsis also works with just the first input', () => {
+  const opts = [
+    number('answer', ['-a', '--answer']),
+    command('help', ['-h', '--help']),
+    flag('version', ['--version'])
+  ]
+
+  const style = {
+    line: {width: 40}
+  }
+
+  const res = synopsis('deepThought')(opts)(style)
+
+  const txt = 'deepThought [-a|--answer] [-h|--help]   \n' +
+              '            [--version]                 \n'
 
   expect(res).toStrictEqual(txt)
 })
