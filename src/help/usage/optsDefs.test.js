c8e8192fb8ac8c50ab0bf5c48aba72000879a760
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-16 19:04:02 +0100

    test: test if optsDefs assumes an empty array if no args are given

--- src/help/usage/optsDefs.test.js
+++ src/help/usage/optsDefs.test.js
@@ -59,3 +59,30 @@ test('optsDefs prints three empty lines if opts has undefined entries', () => {
 
   expect(res).toStrictEqual(txt)
 })
+
+test('optsDefs assumes an empty array if no args are given', () => {
+  const opts = [
+    number('answer', undefined, {desc: 'The answer.'}),
+    command('help', ['-h', '--help'], {desc: 'Prints help.'}),
+    flag('version', ['--version'], {desc: 'Prints version.'})
+  ]
+
+  const style = {
+    line: {width: 40},
+    desc: {padStart: 4, width: 36}
+  }
+  
+  const res = optsDefs(opts)(style)
+
+  const txt = ' [number]                               \n' +
+              '    The answer.                         \n' +
+              '                                        \n' +
+              '-h, --help                              \n' +
+              '    Prints help.                        \n' +
+              '                                        \n' +
+              '--version [flag]                        \n' +
+              '    Prints version.                     \n' +
+              '                                        \n'
+
+  expect(res).toStrictEqual(txt)
+})
