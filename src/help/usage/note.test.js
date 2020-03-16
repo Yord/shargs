a7df063127ffa11d3c7472acffc37cc050a44a9c
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-16 18:27:28 +0100

    test: test if note retains more than one consecutive whitespace even after line breaks

--- src/help/usage/note.test.js
+++ src/help/usage/note.test.js
@@ -16,3 +16,20 @@ test('note generates expected string', () => {
 
   expect(res).toStrictEqual(txt)
 })
+
+test('note retains more than one consecutive whitespace even after line breaks', () => {
+  const opts = []
+  
+  const style = {
+    line: {width: 40}
+  }
+  
+  const res = note(
+    'Deep Thought was created to come up with  the Answer.'
+  )(opts)(style)
+
+  const txt = 'Deep Thought was created to come up with\n' +
+              '  the Answer.                           \n'
+
+  expect(res).toStrictEqual(txt)
+})
