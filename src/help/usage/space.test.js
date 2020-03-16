95da9862900d29544311827207b7d1fb45e11cfa
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-16 20:42:46 +0100

    test: test if spaceFrom correctly passes on id

--- src/help/usage/space.test.js
+++ src/help/usage/space.test.js
@@ -1,4 +1,4 @@
-const {space} = require('./space')
+const {space, spaceFrom} = require('./space')
 
 test('space generates expected string', () => {
   const style = {
@@ -6,6 +6,20 @@ test('space generates expected string', () => {
   }
 
   const res = space(style)
+
+  const txt = '                                        \n'
+
+  expect(res).toStrictEqual(txt)
+})
+
+test('spaceFrom correctly passes on id', () => {
+  const id = 'test'
+  
+  const style = {
+    [id]: {width: 40}
+  }
+
+  const res = spaceFrom(id)(style)
 
   const txt = '                                        \n'
 
