c270bcfaff428c946fb12c1ef2d1dd7031d0e558
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-18 21:23:27 +0100

    test: test if noCommands filters more than one opt

--- src/help/usage/decorators/noCommands.test.js
+++ src/help/usage/decorators/noCommands.test.js
@@ -14,3 +14,15 @@ test('noCommands filters one opt', () => {
 
   expect(res).toStrictEqual(opts.slice(0, 2))
 })
+
+test('noCommands filters more than one opt', () => {
+  const opts = [
+    number('answer', ['-a', '--answer'], {desc: 'The answer.'}),
+    command('help', ['-h', '--help'], {desc: 'Prints help.'}),
+    command('version', ['--version'], {desc: 'Prints version.'})
+  ]
+
+  const res = noCommands(id)(opts)
+
+  expect(res).toStrictEqual(opts.slice(0, 1))
+})
