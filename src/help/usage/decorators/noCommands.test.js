204b5341dc194f28cfe69b7574b1970ecaac65a1
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-18 21:23:11 +0100

    test: test if noCommands filters one opt

---
+++ src/help/usage/decorators/noCommands.test.js
@@ -0,0 +1,16 @@
+const noCommands = require('./noCommands')
+const {command, flag, number} = require('../../../options')
+
+const id = opts => opts
+
+test('noCommands filters one opt', () => {
+  const opts = [
+    number('answer', ['-a', '--answer'], {desc: 'The answer.'}),
+    flag('help', ['-h', '--help'], {desc: 'Prints help.'}),
+    command('version', ['--version'], {desc: 'Prints version.'})
+  ]
+
+  const res = noCommands(id)(opts)
+
+  expect(res).toStrictEqual(opts.slice(0, 2))
+})
