c8d89b7de49696232087f83cd7a9b89c2d0b9f92
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-18 21:47:52 +0100

    test: test if optsFilter does not filter if predicate is undefined

--- src/help/usage/decorators/optsFilter.test.js
+++ src/help/usage/decorators/optsFilter.test.js
@@ -16,3 +16,17 @@ test('optsFilter filters opts', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('optsFilter does not filter if predicate is undefined', () => {
+  const opts = [
+    number('answer', ['-a', '--answer'], {desc: 'The answer.'}),
+    flag('help', ['-h', '--help'], {desc: 'Prints help.'}),
+    command('version', ['--version'], {desc: 'Prints version.'})
+  ]
+
+  const res = optsFilter()(id)(opts)
+
+  const exp = opts
+
+  expect(res).toStrictEqual(exp)
+})
