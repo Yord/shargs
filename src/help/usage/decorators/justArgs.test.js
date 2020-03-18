c1484eb3af531414878894383115eebf3c839bed
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-18 19:50:59 +0100

    test: test if justArgs filters more than one opt

---
+++ src/help/usage/decorators/justArgs.test.js
@@ -0,0 +1,16 @@
+const justArgs = require('./justArgs')
+const {command, flag, number} = require('../../../options')
+
+const id = a => a
+
+test('justArgs filters more than one opt', () => {
+  const opts = [
+    number('answer', ['-a', '--answer'], {desc: 'The answer.'}),
+    command('help', ['-h', '--help'], {desc: 'Prints help.'}),
+    flag('version', ['--version'], {desc: 'Prints version.'})
+  ]
+
+  const res = justArgs(['-a', '-h'])(id)(opts)
+
+  expect(res).toStrictEqual(opts.slice(0, 2))
+})
