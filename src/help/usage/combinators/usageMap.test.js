f33aafb2aa8a9b0c37ce5decfba31954f5865aa6
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-19 11:25:17 +0100

    test: test if usageMap uses default line style if style has no desc attribute

--- src/help/usage/combinators/usageMap.test.js
+++ src/help/usage/combinators/usageMap.test.js
@@ -52,6 +52,32 @@ test('usageMap uses default styles if style is undefined', () => {
   expect(res).toStrictEqual(txt)
 })
 
+test('usageMap uses default line style if style has no desc attribute', () => {
+  const opts = [
+    number('answer', ['-a', '--answer'], {desc: 'The answer.'}),
+    command('help', ['-h', '--help'], {desc: 'Prints help.'}),
+    flag('version', ['--version'], {desc: 'Prints version.'})
+  ]
+
+  const style = {
+    line: {width: 40}
+  }
+
+  const res = usageMap(({args, desc}) => layout([
+    text(args.join(', ')),
+    textFrom('desc')(desc)
+  ]))(opts)(style)
+
+  const txt = '-a, --answer                            \n' +
+              'The answer.                                                                     \n' +
+              '-h, --help                              \n' +
+              'Prints help.                                                                    \n' +
+              '--version                               \n' +
+              'Prints version.                                                                 \n'
+
+  expect(res).toStrictEqual(txt)
+})
+
 test('usageMap returns empty string if opts are undefined', () => {
   const style = {
     line: {width: 40},
