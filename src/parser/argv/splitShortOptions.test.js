c3370b857f2ef23cd2e92ec61a5dcc4fad609ad3
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-19 14:52:18 +0100

    test: test if splitShortOptions does not touch options with two dashes

--- src/parser/argv/splitShortOptions.test.js
+++ src/parser/argv/splitShortOptions.test.js
@@ -9,3 +9,13 @@ test('splitShortOptions splits short options', () => {
 
   expect(argv).toStrictEqual(exp)
 })
+
+test('splitShortOptions does not touch options with two dashes', () => {
+  const obj = {argv: ['--ab']}
+
+  const {argv} = splitShortOptions(obj)
+
+  const exp = ['--ab']
+
+  expect(argv).toStrictEqual(exp)
+})
