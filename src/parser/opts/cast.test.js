90d9aac5fb425ca5a34cf99558518812ebb1c619
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-19 15:37:56 +0100

    test: test if cast does not cast strings

--- src/parser/opts/cast.test.js
+++ src/parser/opts/cast.test.js
@@ -1,4 +1,5 @@
 const cast = require('./cast')
+const {argumentIsNotABool, argumentIsNotANumber} = require('../../errors')
 const {array, bool, command, flag, number, string} = require('../../options')
 
 const numberBool = array(['number', 'bool'])
@@ -24,6 +25,22 @@ test('cast casts values into their types', () => {
     {...command('help', ['-h', '--help']), values: ['foo --bar']},
     {...bool('verbose', ['--verbose']), values: [false]},
     {...flag('version', ['--version']), values: [true]}
+  ]
+
+  expect(opts).toStrictEqual(exp)
+})
+
+test('cast does not cast strings', () => {
+  const obj = {
+    opts: [
+      {...string('title', ['--title']), values: ["The Hitchhiker's Guide to the Galaxy"]}
+    ]
+  }
+
+  const {opts} = cast(obj)
+
+  const exp = [
+    {...string('title', ['--title']), values: ["The Hitchhiker's Guide to the Galaxy"]}
   ]
 
   expect(opts).toStrictEqual(exp)
