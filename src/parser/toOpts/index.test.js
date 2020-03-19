7999f67d3640194cf228b34b92dd479f3ce49935
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-19 17:44:29 +0100

    test: test if toOpts transforms argv into opts

---
+++ src/parser/toOpts/index.test.js
@@ -0,0 +1,52 @@
+const toOpts  = require('./index')
+const combine = require('../combinators/combine')
+const option  = require('../combinators/option')
+const {array, bool, command, flag, number, string} = require('../../options')
+
+const deleteArgs = obj => {
+  delete obj.args
+  return obj
+}
+
+const numberBool = array(['number', 'bool'])
+
+const opts = [
+  string('title', ['--title']),
+  numberBool('numBool', ['-n', '--nb']),
+  number('answer', ['-a', '--answer']),
+  command('help', ['-h', '--help']),
+  bool('verbose', ['--verbose']),
+  flag('version', ['--version'])
+]
+
+const combined = combine(...opts.map(option)).args
+
+test('toOpts transforms argv into opts', () => {
+  const obj = {
+    argv: [
+      'foo',
+      '--title', "The Hitchhiker's Guide to the Galaxy",
+      '-n', '23', 'true',
+      '-a', '42',
+      '--verbose', 'false',
+      '--version',
+      'bar',
+      '-h', 'foo', '--bar'
+    ]
+  }
+
+  const {opts} = toOpts(combined)(obj)
+
+  const exp = [
+    {values: ['foo']},
+    deleteArgs({...string('title', ['--title']), values: ["The Hitchhiker's Guide to the Galaxy"]}),
+    deleteArgs({...numberBool('numBool', ['-n', '--nb']), values: ['23', 'true']}),
+    deleteArgs({...number('answer', ['-a', '--answer']), values: ['42']}),
+    deleteArgs({...bool('verbose', ['--verbose']), values: ['false']}),
+    deleteArgs({...flag('version', ['--version']), values: []}),
+    {values: ['bar']},
+    deleteArgs({...command('help', ['-h', '--help']), values: ['foo', '--bar']})
+  ]
+
+  expect(opts).toStrictEqual(exp)
+})
