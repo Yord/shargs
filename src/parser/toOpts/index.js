44a7a4d5a3927bcd04c450ef22a5e9f3aa579812
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-20 13:32:20 +0100

    chore: move combine inside toOpts

--- src/parser/toOpts/index.js
+++ src/parser/toOpts/index.js
@@ -1,46 +1,54 @@
-module.exports = (args = []) => ({errs = [], argv: ARGV = []} = {}) => {
-  const opts = []
+const combine = require('../combinators/combine')
+const option  = require('../combinators/option')
 
-  let at  = 0
-  let ARG = ARGV[at]
+module.exports = (opts2 = []) => {
+  const {args, errs: ERRS = []} = combine(...opts2.map(option))
 
-  while (ARG) {
-    const options = args[ARG]
+  return ({errs = [], argv: ARGV = []} = {}) => {
+    errs = errs.concat(ERRS)
+    const opts = []
 
-    let at2 = at + 1
+    let at  = 0
+    let ARG = ARGV[at]
 
-    if (options) {
-      for (let j = 0; j < options.length; j++) {
-        const option = options[j]
+    while (ARG) {
+      const options = args[ARG]
 
-        let {types} = option
-        let values  = []
-        if (typeof types === 'undefined' || types === null) {
-          let i = at + 1
-          let arg = ARGV[i] || '--'
-          while (arg !== '--') {
-            if (types === null) types = []
-            types.push('string')
-            values.push(arg)
-            i++
-            arg = ARGV[i] || '--'
+      let at2 = at + 1
+
+      if (options) {
+        for (let j = 0; j < options.length; j++) {
+          const option = options[j]
+
+          let {types} = option
+          let values  = []
+          if (typeof types === 'undefined' || types === null) {
+            let i = at + 1
+            let arg = ARGV[i] || '--'
+            while (arg !== '--') {
+              if (types === null) types = []
+              types.push('string')
+              values.push(arg)
+              i++
+              arg = ARGV[i] || '--'
+            }
+          } else {
+            values = ARGV.slice(at + 1, at + types.length + 1)
           }
-        } else {
-          values = ARGV.slice(at + 1, at + types.length + 1)
+
+          opts.push({...option, values})
+
+          at2 = at + (types === null ? 0 : types.length) + 1
         }
-
-        opts.push({...option, values})
-
-        at2 = at + (types === null ? 0 : types.length) + 1
+      } else {
+        const values = ARGV.slice(at, at + 1)
+        opts.push({values})
       }
-    } else {
-      const values = ARGV.slice(at, at + 1)
-      opts.push({values})
+
+      at  = at2
+      ARG = ARGV[at]
     }
 
-    at  = at2
-    ARG = ARGV[at]
+    return {errs, opts}
   }
-
-  return {errs, opts}
 }
