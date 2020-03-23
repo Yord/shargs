c1464c607fc98e4c72b3ff36accc34f96df300c9
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-23 10:49:20 +0100

    test: test if flagsToBool reports an error if a flag is not a count

--- src/parser/opts/flagsToBool.test.js
+++ src/parser/opts/flagsToBool.test.js
@@ -1,5 +1,6 @@
 const flagsToBool = require('./flagsToBool')
 const {array, bool, command, flag, number, string} = require('../../options')
+const {flagIsNotACount} = require('../../errors')
 
 const numberBool = array(['number', 'bool'])
 
@@ -23,4 +24,29 @@ test('flagsToBool works as expected on all types', () => {
   const exp = obj(versionBool).opts
 
   expect(opts).toStrictEqual(exp)
+})
+
+test('flagsToBool reports an error if a flag is not a count', () => {
+  const versionBool = {...flag('version', ['--version']), values: [true]}
+
+  const obj = version => ({
+    opts: [
+      {...string('title', ['--title']), values: ["The Hitchhiker's Guide to the Galaxy"]},
+      {...numberBool('numBool', ['-n', '--nb']), values: [23, true]},
+      {...number('answer', ['-a', '--answer']), values: [42]},
+      {...command('help', ['-h', '--help']), values: ['foo --bar']},
+      {...bool('verbose', ['--verbose']), values: [false]},
+      ...(typeof version === 'undefined' ? [] : [version])
+    ]
+  })
+
+  const {errs, opts} = flagsToBool(obj(versionBool))
+
+  const expOpts = obj().opts
+  const expErrs = [
+    flagIsNotACount({key: versionBool.key, types: versionBool.types, option: versionBool})
+  ]
+
+  expect(opts).toStrictEqual(expOpts)
+  expect(errs).toStrictEqual(expErrs)
 })
