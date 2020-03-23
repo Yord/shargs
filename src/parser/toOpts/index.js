ee03c70976fb9e8fff85ab92a806a7f55e227645
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-23 12:46:20 +0100

    refactor: rename variables in toOpts to improve signature

--- src/parser/toOpts/index.js
+++ src/parser/toOpts/index.js
@@ -1,12 +1,12 @@
 const combine = require('./combine')
 const option  = require('./option')
 
-module.exports = (opts2 = []) => {
-  const {args, errs: ERRS} = combine(...opts2.map(option))
+module.exports = (opts = []) => {
+  const {args, errs: ERRS} = combine(...opts.map(option))
 
   return ({errs = [], argv: ARGV = []} = {}) => {
     errs = errs.concat(ERRS)
-    const opts = []
+    const opts2 = []
 
     let at  = 0
     let ARG = ARGV[at]
@@ -36,19 +36,19 @@ module.exports = (opts2 = []) => {
             values = ARGV.slice(at + 1, at + types.length + 1)
           }
 
-          opts.push({...option, values})
+          opts2.push({...option, values})
 
           at2 = at + (types === null ? 0 : types.length) + 1
         }
       } else {
         const values = ARGV.slice(at, at + 1)
-        opts.push({values})
+        opts2.push({values})
       }
 
       at  = at2
       ARG = ARGV[at]
     }
 
-    return {errs, opts}
+    return {errs, opts: opts2}
   }
 }
