edab26dbbc771f3940576defe745f5e5d2424a51
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-23 15:43:35 +0100

    test: test if flagAsBool works as expected on all types

--- src/parser/args/flagAsBool.test.js
+++ src/parser/args/flagAsBool.test.js
@@ -15,3 +15,35 @@ test('flagAsBool README example works', () => {
 
   expect(args).toStrictEqual(exp)
 })
+
+test('flagAsBool works as expected on all types', () => {
+  const obj = {
+    args: {
+      title: "The Hitchhiker's Guide to the Galaxy",
+      numBool: [23, true],
+      answer: 42,
+      help: 'foo --bar',
+      verbose: false,
+      version1: {type: 'flag', count: 1},
+      version2: true,
+      no: undefined,
+      nono: null
+    }
+  }
+
+  const {args} = flagAsBool(obj)
+
+  const exp = {
+    title: "The Hitchhiker's Guide to the Galaxy",
+    numBool: [23, true],
+    answer: 42,
+    help: 'foo --bar',
+    verbose: false,
+    version1: true,
+    version2: true,
+    no: undefined,
+    nono: null
+  }
+
+  expect(args).toStrictEqual(exp)
+})
