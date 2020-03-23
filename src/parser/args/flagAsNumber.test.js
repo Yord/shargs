eae24f2d595c8d770b2ce317f76e46883439e14f
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-23 16:52:08 +0100

    test: test if flagAsNumber works as expected on all types

--- src/parser/args/flagAsNumber.test.js
+++ src/parser/args/flagAsNumber.test.js
@@ -15,3 +15,33 @@ test('flagAsNumber README example works', () => {
 
   expect(args).toStrictEqual(exp)
 })
+
+test('flagAsNumber works as expected on all types', () => {
+  const obj = {
+    args: {
+      title: "The Hitchhiker's Guide to the Galaxy",
+      numBool: [23, true],
+      answer: 42,
+      help: 'foo --bar',
+      verbose: false,
+      version: {type: 'flag', count: 1},
+      no: undefined,
+      nono: null
+    }
+  }
+
+  const {args} = flagAsNumber(obj)
+
+  const exp = {
+    title: "The Hitchhiker's Guide to the Galaxy",
+    numBool: [23, true],
+    answer: 42,
+    help: 'foo --bar',
+    verbose: false,
+    version: 1,
+    no: undefined,
+    nono: null
+  }
+
+  expect(args).toStrictEqual(exp)
+})
