862c0c5ec793e3567303114d6f44a9f58d968051
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-23 18:42:03 +0100

    fix: consider cases in which values are undefined or null in the cast opts parser function

--- src/parser/opts/cast.js
+++ src/parser/opts/cast.js
@@ -7,38 +7,42 @@ module.exports = ({errs = [], opts: OPTS = []} = {}) => {
     const option = OPTS[i]
     const {values: VALUES, types} = option
 
-    let values = []
-
-    if (typeof types === 'undefined' || types === null) {
-      values = VALUES
+    if (VALUES === null || typeof VALUES === 'undefined') {
+      opts.push(option)
     } else {
-      if (types.length !== 0) {
-        for (let j = 0; j < types.length; j++) {
-          const type = types[j]
-          const value  = VALUES[j]
-          switch (type) {
-            case 'string':
-              values.push(value)
-              break
-            case 'number':
-              const float = parseFloat(value)
-              if (!Number.isNaN(float)) values.push(float)
-              else errs.push(argumentIsNotANumber({value, option}))
-              break
-            case 'bool':
-              if (value === 'true')       values.push(true)
-              else if (value === 'false') values.push(false)
-              else errs.push(argumentIsNotABool({value, option}))
-              break
-            default:
-              values.push(value)
-              break
+      let values = []
+
+      if (typeof types === 'undefined' || types === null) {
+        values = VALUES
+      } else {
+        if (types.length !== 0) {
+          for (let j = 0; j < types.length; j++) {
+            const type = types[j]
+            const value  = VALUES[j]
+            switch (type) {
+              case 'string':
+                values.push(value)
+                break
+              case 'number':
+                const float = parseFloat(value)
+                if (!Number.isNaN(float)) values.push(float)
+                else errs.push(argumentIsNotANumber({value, option}))
+                break
+              case 'bool':
+                if (value === 'true')       values.push(true)
+                else if (value === 'false') values.push(false)
+                else errs.push(argumentIsNotABool({value, option}))
+                break
+              default:
+                values.push(value)
+                break
+            }
           }
         }
       }
-    }
 
-    opts.push({...option, values, types})
+      opts.push({...option, values})
+    }
   }
 
   return {errs, opts}
