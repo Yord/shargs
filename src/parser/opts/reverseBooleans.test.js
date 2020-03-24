03639bdc45669c9bcd79110c7eb33fa224e314d1
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-24 16:33:07 +0100

    test: test if reverseBooleans does not reverse booleans without values

--- src/parser/opts/reverseBooleans.test.js
+++ src/parser/opts/reverseBooleans.test.js
@@ -34,3 +34,21 @@ test('reverseBooleans does not change flags', () => {
 
   expect(opts).toStrictEqual(exp)
 })
+
+test('reverseBooleans does not reverse booleans without values', () => {
+  const obj = {
+    opts: [
+      bool('bool', ['-b'], {reverse: true, values: [true]}),
+      bool('bool2', ['--b2'], {reverse: true, values: null})
+    ]
+  }
+
+  const {opts} = reverseBooleans(obj)
+
+  const exp = [
+    bool('bool', ['-b'], {reverse: true, values: [false]}),
+    bool('bool2', ['--b2'], {reverse: true, values: null})
+  ]
+
+  expect(opts).toStrictEqual(exp)
+})
