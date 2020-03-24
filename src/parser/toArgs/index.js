5a81a9150a3804d421086c2dd7ae0f894c38f334
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-24 10:48:40 +0100

    feat: filter all options that do not have values before transforming them in toArgs

--- src/parser/toArgs/index.js
+++ src/parser/toArgs/index.js
@@ -2,20 +2,22 @@ module.exports = parser => ({errs = [], opts: OPTS = []} = {}) => {
   const args = {_: []}
 
   for (let i = 0; i < OPTS.length; i++) {
-    const {key, values: argv, types, opts} = OPTS[i]
+    const {key, values, types, opts} = OPTS[i]
 
-    if (typeof types === 'undefined') {
-      if (argv.length !== 1 || argv[0] !== '--') args['_'] = args['_'].concat(argv)
-    } else if (types === null) {
-      const parse = parser(opts || [])
-      const res   = parse(argv, [])
-
-      errs      = errs.concat(res.errs || [])
-      args[key] = Object.assign({}, args[key], res.args)
-    } else if (types.length === 0) {
-      args[key] = {type: 'flag', count: typeof args[key] === 'undefined' ? 1 : args[key].count + 1}
-    } else {
-      args[key] = types.length === 1 ? argv[0] : argv
+    if (Array.isArray(values)) {
+      if (typeof types === 'undefined') {
+        if (values.length !== 1 || values[0] !== '--') args['_'] = args['_'].concat(values)
+      } else if (types === null) {
+        const parse = parser(opts || [])
+        const res   = parse(values, [])
+  
+        errs      = errs.concat(res.errs || [])
+        args[key] = Object.assign({}, args[key], res.args)
+      } else if (types.length === 0) {
+        args[key] = {type: 'flag', count: typeof args[key] === 'undefined' ? 1 : args[key].count + 1}
+      } else {
+        args[key] = types.length === 1 ? values[0] : values
+      }
     }
   }
 
