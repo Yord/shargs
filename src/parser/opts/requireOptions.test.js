bb69f343f6c7c1fc62ec310572d758f2765e7cd8
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-24 11:32:51 +0100

    test: test if requireOptions keeps all options in opts

--- src/parser/opts/requireOptions.test.js
+++ src/parser/opts/requireOptions.test.js
@@ -2,12 +2,28 @@ const requireOptions = require('./requireOptions')
 const {requiredOptionMissing} = require('../../errors')
 const {number} = require('../../options')
 
-
 test('requireOptions README example works', () => {
   const answer = number('answer', ['-a', '--answer'], {required: true})
 
   const obj = {
     opts: [answer]
+  }
+
+  const {errs} = requireOptions(obj)
+
+  const expErrs = [
+    requiredOptionMissing({key: answer.key, args: answer.args, option: answer})
+  ]
+
+  expect(errs).toStrictEqual(expErrs)
+})
+
+test('requireOptions keeps all options in opts', () => {
+  const answer = number('answer', ['-a', '--answer'], {required: true})
+  const question = number('question', ['-q', '--question'], {required: true, values: ['Are default values fine?']})
+
+  const obj = {
+    opts: [answer, question]
   }
 
   const {errs, opts} = requireOptions(obj)
@@ -16,5 +32,8 @@ test('requireOptions README example works', () => {
     requiredOptionMissing({key: answer.key, args: answer.args, option: answer})
   ]
 
+  const expOpts = obj.opts
+
   expect(errs).toStrictEqual(expErrs)
+  expect(opts).toStrictEqual(expOpts)
 })
