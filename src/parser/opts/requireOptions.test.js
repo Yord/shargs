2aaf54788b468af0ac161aa9af4d48c7d4de3b30
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-24 11:33:42 +0100

    test: test if requireOptions works as expected on all types

--- src/parser/opts/requireOptions.test.js
+++ src/parser/opts/requireOptions.test.js
@@ -1,6 +1,8 @@
 const requireOptions = require('./requireOptions')
-const {requiredOptionMissing} = require('../../errors')
-const {number} = require('../../options')
+const {requiredOptionFormat, requiredOptionMissing} = require('../../errors')
+const {array, bool, command, flag, number, string} = require('../../options')
+
+const numberBool = array(['number', 'bool'])
 
 test('requireOptions README example works', () => {
   const answer = number('answer', ['-a', '--answer'], {required: true})
@@ -36,4 +38,25 @@ test('requireOptions keeps all options in opts', () => {
 
   expect(errs).toStrictEqual(expErrs)
   expect(opts).toStrictEqual(expOpts)
+})
+
+test('requireOptions works as expected on all types', () => {
+  const obj = {
+    opts: [
+      string('title', ['--title'], {required: true}),
+      numberBool('numBool', ['-n', '--nb'], {required: true}),
+      number('answer', ['-a', '--answer'], {required: true}),
+      command('help', ['-h', '--help'], {required: true}),
+      bool('verbose', ['--verbose'], {required: true}),
+      flag('version', ['--version'], {required: true})
+    ]
+  }
+
+  const {errs} = requireOptions(obj)
+
+  const exp = obj.opts.map(option =>
+    requiredOptionMissing({key: option.key, args: option.args, option})
+  )
+
+  expect(errs).toStrictEqual(exp)
 })
