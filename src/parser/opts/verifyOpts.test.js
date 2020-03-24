e23e30306f1aeaeb22459f89159c6dcc9ef56b73
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-24 22:17:46 +0100

    test: test if verifyOpts README example works

---
+++ src/parser/opts/verifyOpts.test.js
@@ -0,0 +1,25 @@
+const verifyOpts = require('./verifyOpts')
+const {falseOptsRules} = require('../../errors')
+const {string} = require('../../options')
+
+test('verifyOpts README example works', () => {
+  const implies = (p, q) => !p || q
+
+  const rules = opts => implies(
+    opts.some(({key, values}) => key === 'firstName' && values !== null),
+    opts.some(({key, values}) => key === 'lastName' && values !== null)
+  )
+
+  const opts = [
+    string('firstName', ['-f'], {values: ['Charles']}),
+    string('lastName', ['-l'])
+  ]
+
+  const {errs} = verifyOpts(rules)({opts})
+
+  const exp = [
+    falseOptsRules({rules, options: opts})
+  ]
+
+  expect(errs).toStrictEqual(exp)
+})
