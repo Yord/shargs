1a6382f68c2b191caec8a4aa15472b8c7ea32464
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-25 10:14:45 +0100

    test: test if demandACommand records no error if a command is defined

--- src/parser/opts/demandACommand.test.js
+++ src/parser/opts/demandACommand.test.js
@@ -1,6 +1,6 @@
 const demandACommand = require('./demandACommand')
 const {commandRequired} = require('../../errors')
-const {array, bool, flag, number, string} = require('../../options')
+const {array, bool, command, flag, number, string} = require('../../options')
 
 const numberBool = array(['number', 'bool'])
 
@@ -18,6 +18,23 @@ test('demandACommand README example works', () => {
   const exp = [
     commandRequired({options: opts})
   ]
+
+  expect(errs).toStrictEqual(exp)
+})
+
+test('demandACommand records no error if a command is defined', () => {
+  const opts = [
+    string('title', ['--title'], {values: ["The Hitchhiker's Guide to the Galaxy"]}),
+    numberBool('numBool', ['-n', '--nb'], {values: ['23', 'true']}),
+    number('answer', ['-a', '--answer'], {values: ['42']}),
+    command('help', ['-h', '--help'], {values: ['foo', '--bar']}),
+    bool('verbose', ['--verbose'], {values: ['false']}),
+    flag('version', ['--version'], {values: [1]})
+  ]
+
+  const {errs} = demandACommand({opts})
+
+  const exp = []
 
   expect(errs).toStrictEqual(exp)
 })
