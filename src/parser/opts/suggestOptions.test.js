b5593324b036c1de5fcda17be602f8ee2c2b8ecb
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-25 18:50:32 +0100

    test: test if suggestOptions works as expected

--- src/parser/opts/suggestOptions.test.js
+++ src/parser/opts/suggestOptions.test.js
@@ -1,6 +1,6 @@
 const suggestOptions = require('./suggestOptions')
 const {didYouMean} = require('../../errors')
-const {number} = require('../../options')
+const {bool, number, string} = require('../../options')
 
 test('suggestOptions README example works', () => {
   const age = number('age', ['-a', '--age'])
@@ -20,6 +20,65 @@ test('suggestOptions README example works', () => {
         [],
         [{'--age': age}],
         [{'-a': age}]
+      ]
+    })
+  ]
+
+  expect(errs).toStrictEqual(exp)
+})
+
+test('suggestOptions works as expected', () => {
+  const argv1 = '--titel'
+  const argv2 = '--titl'
+  const argv3 = '--beer'
+  const title = string('title', ['-t', '--title'])
+  const age   = number('age', ['--age', '-a'])
+  const beef  = bool('age', ['--beef', '-b'])
+
+  const opts = [
+    beef,
+    {values: [argv1]},
+    title,
+    {values: [argv2]},
+    {values: [argv3]},
+    age
+  ]
+
+  const {errs} = suggestOptions({opts})
+
+  const exp = [
+    didYouMean({
+      argv: argv1,
+      options: [
+        [],
+        [],
+        [{'--title': title}],
+        [],
+        [{'--beef': beef}, {'--age': age}],
+        [{'-t': title}],
+        [{'-b': beef}, {'-a': age}]
+      ]
+    }),
+    didYouMean({
+      argv: argv2,
+      options: [
+        [],
+        [{'--title': title}],
+        [],
+        [],
+        [{'--beef': beef}, {'-t': title}, {'--age': age}],
+        [{'-b': beef}, {'-a': age}]
+      ]
+    }),
+    didYouMean({
+      argv: argv3,
+      options: [
+        [],
+        [{'--beef': beef}],
+        [],
+        [{'--age': age}],
+        [{'-b': beef}],
+        [{'-t': title}, {'--title': title}, {'-a': age}]
       ]
     })
   ]
