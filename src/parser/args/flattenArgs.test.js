42868e6f2270d4da8ec5f25dd632c4e2ba92744a
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-25 11:49:53 +0100

    test: test if flattenArgs takes custom merge functions 1/2

--- src/parser/args/flattenArgs.test.js
+++ src/parser/args/flattenArgs.test.js
@@ -25,3 +25,31 @@ test('flattenArgs README example works', () => {
 
   expect(args).toStrictEqual(exp)
 })
+
+test('flattenArgs takes custom merge functions 1/2', () => {
+  const obj = {
+    args: {
+      version: {type: 'flag', count: 2},
+      name: 'Logan',
+      command: {
+        version: {type: 'flag', count: 1},
+        name: 'Charles',
+        help: true
+      },
+      verbose: true
+    }
+  }
+
+  const mergeLeft  = (obj1, obj2) => ({...obj2, ...obj1})
+
+  const {args} = flattenArgs(mergeLeft)(obj)
+
+  const exp = {
+    version: {type: 'flag', count: 2},
+    name: 'Logan',
+    help: true,
+    verbose: true
+  }
+
+  expect(args).toStrictEqual(exp)
+})
