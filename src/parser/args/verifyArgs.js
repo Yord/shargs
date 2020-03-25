1c9de1a838daef062abb97aa707f05ef7b837781
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-25 06:06:06 +0100

    feat: introduce verifyArgs args parser function

---
+++ src/parser/args/verifyArgs.js
@@ -0,0 +1,15 @@
+const {falseArgsRules, wrongArgsRulesType} = require('../../errors')
+
+module.exports = (rules = () => true) => ({errs = [], args = []} = {}) => {
+  const errs2 = []
+
+  if (typeof rules === 'function') {
+    if (rules(args) === false) {
+      errs2.push(falseArgsRules({rules, args}))
+    }
+  } else {
+    errs2.push(wrongArgsRulesType({type: typeof rules, args}))
+  }
+
+  return {errs: errs.concat(errs2), args}
+}
