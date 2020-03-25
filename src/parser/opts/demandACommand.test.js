47d484f72fb2ab1bc436f04f8a3b75abc67c5528
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-25 10:14:27 +0100

    test: test if demandACommand README example works

---
+++ src/parser/opts/demandACommand.test.js
@@ -0,0 +1,23 @@
+const demandACommand = require('./demandACommand')
+const {commandRequired} = require('../../errors')
+const {array, bool, flag, number, string} = require('../../options')
+
+const numberBool = array(['number', 'bool'])
+
+test('demandACommand README example works', () => {
+  const opts = [
+    string('title', ['--title'], {values: ["The Hitchhiker's Guide to the Galaxy"]}),
+    numberBool('numBool', ['-n', '--nb'], {values: ['23', 'true']}),
+    number('answer', ['-a', '--answer'], {values: ['42']}),
+    bool('verbose', ['--verbose'], {values: ['false']}),
+    flag('version', ['--version'], {values: [1]})
+  ]
+
+  const {errs} = demandACommand({opts})
+
+  const exp = [
+    commandRequired({options: opts})
+  ]
+
+  expect(errs).toStrictEqual(exp)
+})
