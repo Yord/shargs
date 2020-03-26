c4e4ac1cb9353566bdd97fd983dda430b2068e73
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-26 16:29:40 +0100

    test: test if parser with only verifyArgs works as expected

--- src/parser/parser.integration.test.js
+++ src/parser/parser.integration.test.js
@@ -561,3 +561,31 @@ test('parser with only transformArgs works as expected', () => {
   expect(args).toStrictEqual(expArgs)
   expect(errs.map(noInfo)).toStrictEqual(expErrs.map(noInfo))
 })
+
+test('parser with only verifyArgs works as expected', () => {
+  const rules = args => !args.query || args.query.indexOf('Terminator') > -1
+
+  const stages = {
+    args: [verifyArgs(rules)]
+  }
+
+  const {errs, args} = parser(stages)(opts)(argv)
+
+  const expArgs = {
+    _: ['--colors', '-vv'],
+    fantasy: 'true',
+    popcorn: {type: 'flag', count: 1},
+    rate: {
+      _: [],
+      stars: '8'
+    },
+    query: 'Supersize Me'
+  }
+
+  const expErrs = [
+    falseArgsRules({})
+  ]
+
+  expect(args).toStrictEqual(expArgs)
+  expect(errs.map(noInfo)).toStrictEqual(expErrs.map(noInfo))
+})
