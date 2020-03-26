9edabcb7a4e519aa6dcda2c9a2766794f3f2abb8
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-26 16:27:18 +0100

    test: test if parser with only verifyRules works as expected

--- src/parser/parser.integration.test.js
+++ src/parser/parser.integration.test.js
@@ -333,3 +333,29 @@ test('parser with only verifyOpts works as expected', () => {
   expect(args).toStrictEqual(expArgs)
   expect(errs.map(noInfo)).toStrictEqual(expErrs.map(noInfo))
 })
+
+test('parser with only verifyRules works as expected', () => {
+  const stages = {
+    opts: [verifyRules]
+  }
+
+  const {errs, args} = parser(stages)(opts)(argv)
+
+  const expArgs = {
+    _: ['--colors', '-vv'],
+    fantasy: 'true',
+    popcorn: {type: 'flag', count: 1},
+    rate: {
+      _: [],
+      stars: '8'
+    },
+    query: 'Supersize Me'
+  }
+
+  const expErrs = [
+    falseRules({})
+  ]
+
+  expect(args).toStrictEqual(expArgs)
+  expect(errs.map(noInfo)).toStrictEqual(expErrs.map(noInfo))
+})
