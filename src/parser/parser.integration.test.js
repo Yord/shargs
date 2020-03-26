55e849136933aa63e18c7fb4775a66f2de621225
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-26 16:28:56 +0100

    test: test if parser with only flagsAsNumbers works as expected

--- src/parser/parser.integration.test.js
+++ src/parser/parser.integration.test.js
@@ -485,3 +485,27 @@ test('parser with only flagsAsBools works as expected', () => {
   expect(args).toStrictEqual(expArgs)
   expect(errs.map(noInfo)).toStrictEqual(expErrs.map(noInfo))
 })
+
+test('parser with only flagsAsNumbers works as expected', () => {
+  const stages = {
+    args: [flagsAsNumbers]
+  }
+
+  const {errs, args} = parser(stages)(opts)(argv)
+
+  const expArgs = {
+    _: ['--colors', '-vv'],
+    fantasy: 'true',
+    popcorn: 1,
+    rate: {
+      _: [],
+      stars: '8'
+    },
+    query: 'Supersize Me'
+  }
+
+  const expErrs = []
+
+  expect(args).toStrictEqual(expArgs)
+  expect(errs.map(noInfo)).toStrictEqual(expErrs.map(noInfo))
+})
