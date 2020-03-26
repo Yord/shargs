577df8a6d8db348f0727ef024a3d582341f20af1
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-26 16:26:11 +0100

    test: test if parser with only reverseBools works as expected

--- src/parser/parser.integration.test.js
+++ src/parser/parser.integration.test.js
@@ -227,3 +227,27 @@ test('parser with only restrictToOnly works as expected', () => {
   expect(args).toStrictEqual(expArgs)
   expect(errs.map(noInfo)).toStrictEqual(expErrs.map(noInfo))
 })
+
+test('parser with only reverseBools works as expected', () => {
+  const stages = {
+    opts: [reverseBools]
+  }
+
+  const {errs, args} = parser(stages)(opts)(argv)
+
+  const expArgs = {
+    _: ['--colors', '-vv'],
+    fantasy: 'false',
+    popcorn: {type: 'flag', count: 1},
+    rate: {
+      _: [],
+      stars: '8'
+    },
+    query: 'Supersize Me'
+  }
+
+  const expErrs = []
+
+  expect(args).toStrictEqual(expArgs)
+  expect(errs.map(noInfo)).toStrictEqual(expErrs.map(noInfo))
+})
