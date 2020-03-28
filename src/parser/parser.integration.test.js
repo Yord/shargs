be93dc74469bf28eb9d17db9191a8124e2ad4bfa
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-28 19:31:30 +0100

    test: test if parser without stages works as expected

---
+++ src/parser/parser.integration.test.js
@@ -0,0 +1,95 @@
+const parser            = require('./combinators/parser')
+
+const splitShortOptions = require('./argv/splitShortOptions')
+const verifyArgv        = require('./argv/verifyArgv')
+const bestGuessOpts     = require('./opts/bestGuessOpts')
+const cast              = require('./opts/cast')
+const demandACommand    = require('./opts/demandACommand')
+const requireOptions    = require('./opts/requireOptions')
+const restrictToOnly    = require('./opts/restrictToOnly')
+const reverseBools      = require('./opts/reverseBools')
+const reverseFlags      = require('./opts/reverseFlags')
+const suggestOptions    = require('./opts/suggestOptions')
+const verifyOpts        = require('./opts/verifyOpts')
+const verifyRules       = require('./opts/verifyRules')
+const verifyValuesArity = require('./opts/verifyValuesArity')
+const bestGuessRest     = require('./args/bestGuessRest')
+const clearRest         = require('./args/clearRest')
+const failRest          = require('./args/failRest')
+const flagsAsBools      = require('./args/flagsAsBools')
+const flagsAsNumbers    = require('./args/flagsAsNumbers')
+const mergeArgs         = require('./args/mergeArgs')
+const transformArgs     = require('./args/transformArgs')
+const verifyArgs        = require('./args/verifyArgs')
+
+const {bool, command, flag, number, string} = require('../options')
+const {argumentIsNotANumber, commandRequired, didYouMean, falseArgsRules, falseArgvRules, falseOptsRules, falseRules, invalidValues, requiredOptionMissing, unexpectedArgument, valueRestrictionsViolated} = require('../errors')
+
+const noCommands = opts => opts.filter(({types}) => types !== null)
+
+const filterErrs = keys => errs => errs.map(
+  ({info, ...rest}) => Object.keys(info).reduce(
+    (acc, key) => ({
+      ...acc,
+      info: {
+        ...acc.info,
+        ...(keys.indexOf(key) === -1 ? {[key]: info[key]} : {[key]: undefined})
+      }
+    }),
+    {info: {}, ...rest}
+  )
+)
+
+const opts = [
+  flag('help', ['--help']),
+  flag('verbose', ['-v', '--verbose']),
+  flag('popcorn', ['-l', '--low-fat'], {reverse: true}),
+  bool('fantasy', ['-E', '--no-hobbits'], {reverse: true}),
+  string('genre', ['-g', '--genre'], {required: true}),
+  number('hours', ['-h', '--hours'], {values: 2}),
+  command('rate', ['rate'], {opts: [
+    number('stars', ['-s', '--stars'], {only: ['1', '2', '3', '4', '5']})
+  ]}),
+  string('query', ['-q', '--query'], {
+    rules: title => opts => (
+      !title.values[0].includes('Supersize Me') ||
+      opts.some(_ => _.key === 'popcorn' && _.values.count === 0)
+    )
+  })
+]
+
+const argv = [
+  '--query', 'Supersize Me',
+  '--colors',
+  '-l',
+  '--no-hobbits', 'true',
+  '-vv',
+  'rate',
+    '--stars', '8',
+    '--help'
+]
+
+test('parser without stages works as expected', () => {
+  const stages = {}
+
+  const {errs, args} = parser(stages)(opts)(argv)
+
+  const expArgs = {
+    _: ['--colors', '-vv'],
+    fantasy: 'true',
+    help: {type: 'flag', count: 1},
+    popcorn: {type: 'flag', count: 1},
+    rate: {
+      _: ['--help'],
+      stars: '8'
+    },
+    query: 'Supersize Me'
+  }
+
+  const expErrs = []
+
+  const errs2 = filterErrs([])(errs)
+
+  expect(args).toStrictEqual(expArgs)
+  expect(errs2).toStrictEqual(expErrs)
+})
