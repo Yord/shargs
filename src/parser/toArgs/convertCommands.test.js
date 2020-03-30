181f29795c67d1c73a17909c2f4152054c4b952f
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-30 11:38:00 +0200

    test: test if convertCommands works as expected if parent parser is not set

--- src/parser/toArgs/convertCommands.test.js
+++ src/parser/toArgs/convertCommands.test.js
@@ -34,3 +34,35 @@ test('convertCommands works as expected', () => {
   expect(args).toStrictEqual(expArgs)
   expect(errs).toStrictEqual(expErrs)
 })
+
+test('convertCommands works as expected if parent parser is not set', () => {
+  const opts = [
+    string('name', ['--name']),
+    {
+      ...command('ask', ['ask'], {
+        opts: [
+          string('question', ['-q']),
+          flag('jokingly', ['-j'], {defaultValue: [1]})
+        ]
+      }),
+      values: ['-q', "What's your lastname?", '--name', 'Logan']
+    }
+  ]
+
+  const parsers = {_: parser()}
+
+  const {errs, args} = convertCommands(parsers)({opts})
+
+  const expArgs = {
+    _: [],
+    ask: {
+      _: ['--name', 'Logan'],
+      question: "What's your lastname?"
+    }
+  }
+
+  const expErrs = []
+
+  expect(args).toStrictEqual(expArgs)
+  expect(errs).toStrictEqual(expErrs)
+})
