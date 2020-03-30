47fa9ae1cc33ef2507044c77ba4b53b038fa5b9f
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-30 15:46:47 +0200

    test: test if implyOpts README example works

---
+++ src/parser/opts/implyOpts.test.js
@@ -0,0 +1,20 @@
+const implyOpts = require('./implyOpts')
+const {implicationViolated, wrongImpliesType} = require('../../errors')
+const {number, string} = require('../../options')
+
+test('implyOpts README example works', () => {
+  const age      = {...number('age', ['-a'], {implies: ['birthday']}), values: ['27']}
+  const birthday = string('birthday', ['-b'], {implies: ['age']})
+
+  const obj = {
+    opts: [age, birthday]
+  }
+
+  const {errs} = implyOpts(obj)
+
+  const exp = [
+    implicationViolated({key: 'age', implies: ['birthday'], option: age})
+  ]
+
+  expect(errs).toStrictEqual(exp)
+})
