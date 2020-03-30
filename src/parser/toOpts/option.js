4367494d64bba0d894205f373ee097fcfeb1af75
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-30 14:23:05 +0200

    feat: throw illegal key name error in option if the key's name is underscore

--- src/parser/toOpts/option.js
+++ src/parser/toOpts/option.js
@@ -1,13 +1,17 @@
+const {illegalKeyName} = require('../../errors')
+
 module.exports = (opt = {}) => {
+  const errs = []
+  const args = {}
+
   const {
     key = null,
     args: ARGS = []
   } = opt
 
-  const errs = []
-  const args = {}
-  
-  if (key !== null && ARGS !== null && ARGS.length > 0) {
+  if (key === '_') {
+    errs.push(illegalKeyName({key, option: opt}))
+  } else if (key !== null && ARGS !== null && ARGS.length > 0) {
     for (let i = 0; i < ARGS.length; i++) {
       const arg  = ARGS[i]
       if (typeof args[arg] === 'undefined') args[arg] = []
