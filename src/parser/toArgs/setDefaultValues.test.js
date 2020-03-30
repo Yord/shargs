1e5e7bddf1b6f7a2d20f92abc148199c34bef86c
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-30 11:39:48 +0200

    test: test if setDefaultValues works as expected

---
+++ src/parser/toArgs/setDefaultValues.test.js
@@ -0,0 +1,27 @@
+const setDefaultValues   = require('./setDefaultValues')
+const {command, flag, string} = require('../../options')
+
+test('setDefaultValues works as expected', () => {
+  const opts = [
+    string('name', ['--name'], {defaultValues: ['Logan']}),
+    command('ask', ['ask'], {
+      opts: [string('question', ['-q']), flag('jokingly', ['-j'], {defaultValues: [1]})],
+      defaultValues: {jokingly: true}
+    })
+  ]
+
+  const {errs, args} = setDefaultValues({opts})
+
+  const expArgs = {
+    _: [],
+    name: 'Logan',
+    ask: {
+      jokingly: true
+    }
+  }
+
+  const expErrs = []
+
+  expect(args).toStrictEqual(expArgs)
+  expect(errs).toStrictEqual(expErrs)
+})
