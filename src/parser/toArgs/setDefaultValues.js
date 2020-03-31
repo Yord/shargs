feb23c513ac92cc8d898710b150eac5503f29189
Philipp Wille <philipp.y.wille@gmail.com>
2020-03-31 13:24:29 +0200

    feat: align setDefaultValues with values behaviour in that defaultValues must have the same arity as their types

--- src/parser/toArgs/setDefaultValues.js
+++ src/parser/toArgs/setDefaultValues.js
@@ -1,13 +1,20 @@
+const {invalidDefaultValues} = require('../../errors')
+
 module.exports = ({errs = [], opts: OPTS = []} = {}) => {
   let args  = {_: []}
   let errs2 = []
 
   for (let i = 0; i < OPTS.length; i++) {
-    const {key, types, values, defaultValues} = OPTS[i]
+    const opt = OPTS[i]
+    const {key, types, values, defaultValues} = opt
 
     if (typeof values === 'undefined' && typeof defaultValues !== 'undefined') {
-      if (Array.isArray(types) && types.length === 1) {
-        args[key] = Array.isArray(defaultValues) && defaultValues.length === 1 ? defaultValues[0] : defaultValues
+      if (Array.isArray(types)) {
+        if (Array.isArray(defaultValues) && defaultValues.length === types.length) {
+          args[key] = defaultValues.length === 1 ? defaultValues[0] : defaultValues
+        } else {
+          errs2.push(invalidDefaultValues({defaultValues, option: opt}))
+        }
       } else {
         args[key] = defaultValues
       }
