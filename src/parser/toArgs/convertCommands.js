7cafa0aebfe34055dcdf8f7aeda547eb32d6298e
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-02 18:14:31 +0200

    feat: add positional arguments to convertCommands

--- src/parser/toArgs/convertCommands.js
+++ src/parser/toArgs/convertCommands.js
@@ -1,22 +1,53 @@
+const {invalidRequiredPositionalArgument, invalidVariadicPositionalArgument, requiredPositionalArgumentMissing} = require('../../errors')
+
 module.exports = parsers => ({errs = [], opts: OPTS = []} = {}) => {
   let args  = {_: []}
   let errs2 = []
 
   for (let i = 0; i < OPTS.length; i++) {
-    const {key, values, types, opts} = OPTS[i]
+    const {key, values, types, opts, posArgs} = OPTS[i]
 
     if (Array.isArray(values) && types === null) {
       const parentParser = parsers.__
       const childParser  = typeof parsers[key] === 'function' ? parsers[key] : parsers._
 
-      const child = childParser(opts || [])(values, [])
-      errs2       = errs2.concat(child.errs || [])
-      args[key]   = Object.assign({}, args[key], child.args)
+      if (typeof args[key] === 'undefined') {
+        args[key] = {}
 
-      if (typeof parentParser === 'function') {
-        const parent = parentParser(filter(OPTS))(child.args._, [])
-        errs2        = errs2.concat(parent.errs || [])
-        args         = {...parent.args, ...args, _: args._.concat(parent.args._)}
+        const child = childParser(opts || [])(values, [])
+
+        if (Array.isArray(posArgs)) {
+          const errs3 = validatePositionalArguments(posArgs)
+          errs2       = errs2.concat(errs3)
+
+          for (let j = 0; j < posArgs.length; j++) {
+            const posArg = posArgs[j]
+            const {key: key2, types: types2, required = false, variadic = false} = posArg
+
+            const values2 = child.args._.slice(0, variadic === true ? child.args._.length : types2.length)
+
+            if (types2 === null && required === true && values2.length === 0) {
+              errs2.push(requiredPositionalArgumentMissing({key: key2, positionalArgument: posArg}))
+            } else if (types2 === null || values2.length === types2.length) {
+               if (typeof args[key][key2] === 'undefined') {
+                 args[key][key2] = values2.length === 1 ? values2[0] : values2
+               }
+
+               child.args._ = child.args._.slice(types2 === null ? child.args._.length : types2.length)
+            } else {
+              errs2.push(requiredPositionalArgumentMissing({key: key2, positionalArgument: posArg}))
+            }
+          }
+        }
+
+        errs2       = errs2.concat(child.errs || [])
+        args[key]   = Object.assign({}, args[key], child.args)
+
+        if (typeof parentParser === 'function') {
+          const parent = parentParser(filter(OPTS))(child.args._, [])
+          errs2        = errs2.concat(parent.errs || [])
+          args         = {...parent.args, ...args, _: args._.concat(parent.args._)}
+        }
       }
     }
   }
@@ -38,4 +69,38 @@ function isNotRest ({types}) {
 
 function hasNoValues ({values}) {
   return typeof values === 'undefined'
+}
+
+function validatePositionalArguments (posArgs) {
+  const errs = []
+
+  const invalidRequired = posArgs.reduce(
+    (bool, {required = false}) => {
+      if (required === false || required === true) {
+        if (bool === null) {
+          return required ? null : false
+        } else {
+          return bool || required
+        }
+      } else {
+        return true
+      }
+    },
+    null
+  )
+
+  if (invalidRequired === true) {
+    errs.push(invalidRequiredPositionalArgument({positionalArguments: posArgs}))
+  }
+
+  const invalidVariadic = posArgs.slice(0, posArgs.length - 1).reduce(
+    (bool, {variadic = false}) => bool || variadic,
+    false
+  )
+
+  if (invalidVariadic === true) {
+    errs.push(invalidVariadicPositionalArgument({positionalArguments: posArgs}))
+  }
+
+  return errs
 }
