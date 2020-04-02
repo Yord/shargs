b04317da5bccfe641df42143abe26c71dd584978
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-02 13:43:05 +0200

    test: test if bestGuessCast README example works

---
+++ src/parser/args/bestGuessCast.test.js
@@ -0,0 +1,47 @@
+const bestGuessCast = require('./bestGuessCast')
+
+test('bestGuessCast README example works', () => {
+  const obj = {
+    args: {
+      _: ['--name', 'Logan'],
+      str1: 'yay',
+      num1: '42.3',
+      num2: '123e-1',
+      num3: '0x11',
+      num4: '0b11',
+      bool1: 'true',
+      arr1: ['-42', 'true', 'yay'],
+      obj: {
+        num5: '0o11',
+        num6: '-Infinity',
+        num7: '',
+        num8: null,
+        bool2: 'false',
+        bool3: undefined
+      }
+    }
+  }
+
+  const {args} = bestGuessCast(obj)
+
+  const exp = {
+    _: ['--name', 'Logan'],
+    str1: 'yay',
+    num1: 42.3,
+    num2: 12.3,
+    num3: 17,
+    num4: 3,
+    bool1: true,
+    arr1: [-42, true, 'yay'],
+    obj: {
+      num5: 9,
+      num6: -Infinity,
+      num7: '',
+      num8: null,
+      bool2: false,
+      bool3: undefined
+    }
+  }
+
+  expect(args).toStrictEqual(exp)
+})
