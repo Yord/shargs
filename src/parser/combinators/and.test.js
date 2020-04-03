0c0af6236f357e69012637ed1452915a6f8af9aa
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-03 12:59:23 +0200

    test: test if and follows right identity

--- src/parser/combinators/and.test.js
+++ src/parser/combinators/and.test.js
@@ -3,6 +3,8 @@ const and = require('./and')
 
 const pred = () => func(boolean())
 
+const T = () => true
+
 test('and is associative', () => {
   const p = pred()
   const q = pred()
@@ -14,6 +16,20 @@ test('and is associative', () => {
         and(p, and(q, r))(a)
       ).toStrictEqual(
         and(and(p, q), r)(a)
+      )
+    })
+  )
+})
+
+test('and follows right identity', () => {
+  const p = pred()
+  const a = anything()
+  assert(
+    property(p, a, (p, a) => {
+      expect(
+        and(p, T)(a)
+      ).toStrictEqual(
+        p(a)
       )
     })
   )
