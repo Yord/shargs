7002fd0e6d46768bb10c70a4f67d54af32e71c51
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-03 13:00:10 +0200

    refactor: use and in broadenBools

--- src/parser/opts/broadenBools.js
+++ src/parser/opts/broadenBools.js
@@ -1,6 +1,7 @@
 const traverseOpts = require('./traverseOpts')
 const {invalidBoolMapping} = require('../../errors')
 const pipe = require('../combinators/pipe')
+const and  = require('../combinators/and')
 
 module.exports = (alt = {}) => {
   const altToBool = reverse(alt)
@@ -12,7 +13,7 @@ module.exports = (alt = {}) => {
 }
 
 function broadenValues (key, altToBool, alt) {
-  return traverseOpts(opt => hasBool(opt) && validValues(key)(opt))(opt => {
+  return traverseOpts(and(hasBool, validValues(key)))(opt => {
     let errs   = []
 
     const {types, [key]: values} = opt
