3a4f135d57c8f5bb4ac42c3ad4a282e185145a56
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-03 12:00:39 +0200

    refactor: define cast in terms of transformOpts

--- src/parser/opts/cast.js
+++ src/parser/opts/cast.js
@@ -1,64 +1,65 @@
+const transformOpts = require('./transformOpts')
 const {argumentIsNotABool, argumentIsNotANumber} = require('../../errors')
+const pipe = require('../combinators/pipe')
 
-module.exports = ({errs = [], opts: OPTS = []} = {}) => {
-  let errs2 = []
-  const opts = []
+module.exports = pipe(
+  cast('values'),
+  cast('defaultValues')
+)
 
-  for (let i = 0; i < OPTS.length; i++) {
-    const option = OPTS[i]
-    
-    const {errs: errs3, option: option2} = transformOption(option, 'values')
-    const {errs: errs4, option: option3} = transformOption(option2, 'defaultValues')
-
-    errs2 = errs2.concat(errs3).concat(errs4)
-    opts.push(option3)
-  }
-
-  return {errs: errs.concat(errs2), opts}
+function cast (key) {
+  return transformOpts(opt => hasValues(key)(opt) && hasTypes(opt) && !isCommand(opt) && !isFlag(opt))(castKey(key))
 }
 
-function transformOption (option, key) {
-  const errs  = []
-  let option2 = option
-
-  const {[key]: VALUES, types} = option
-
-  if (VALUES === null || typeof VALUES === 'undefined') {
-    option2 = option
-  } else {
-    let values = []
-
-    if (typeof types === 'undefined' || types === null) {
-      values = VALUES
-    } else if (Array.isArray(types) && types.length === 0) {
-      values = VALUES
-    } else {
-      for (let j = 0; j < types.length; j++) {
-        const type = types[j]
-        const value  = VALUES[j]
-        switch (type) {
-          case 'string':
-            values.push(value)
-            break
-          case 'number':
-            const float = parseFloat(value)
-            if (!Number.isNaN(float)) values.push(float)
-            else errs.push(argumentIsNotANumber({[key]: VALUES, index: j, option}))
-            break
-          case 'bool':
-            if (value === 'true' || value === true)        values.push(true)
-            else if (value === 'false' || value === false) values.push(false)
-            else errs.push(argumentIsNotABool({[key]: VALUES, index: j, option}))
-            break
-          default:
-            values.push(value)
-            break
-        }
+function castKey (key) {
+  return opt => {
+    const errs  = []
+    const opts = []
+  
+    const {[key]: values, types} = opt
+  
+    let values2 = []
+  
+    for (let j = 0; j < types.length; j++) {
+      const type  = types[j]
+      const value = values[j]
+      switch (type) {
+        case 'string':
+          values2.push(value)
+          break
+        case 'number':
+          const float = parseFloat(value)
+          if (!Number.isNaN(float)) values2.push(float)
+          else errs.push(argumentIsNotANumber({[key]: values, index: j, option: opt}))
+          break
+        case 'bool':
+          if (value === 'true' || value === true)        values2.push(true)
+          else if (value === 'false' || value === false) values2.push(false)
+          else errs.push(argumentIsNotABool({[key]: values, index: j, option: opt}))
+          break
+        default:
+          values2.push(value)
+          break
       }
     }
-
-    option2 = {...option, ...(values.length === 0 ? {} : {[key]: values})}
+    opts.push({...opt, ...(values2.length === 0 ? {} : {[key]: values2})})
+  
+    return {errs, opts}
   }
+}
 
-  return {errs, option: option2}
+function hasTypes ({types}) {
+  return typeof types !== 'undefined'
+}
+
+function isCommand ({types}) {
+  return types === null
+}
+
+function isFlag ({types}) {
+  return Array.isArray(types) && types.length === 0
+}
+
+function hasValues (key) {
+  return ({[key]: values}) => values !== null && typeof values !== 'undefined'
 }
