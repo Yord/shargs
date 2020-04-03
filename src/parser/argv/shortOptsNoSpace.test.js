9dceb3e184189dfa11e793cedc3e52dad0df0bf8
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-03 15:49:25 +0200

    test: test if shortOptsNoSpace does not touch options without single minus sign

--- src/parser/argv/shortOptsNoSpace.test.js
+++ src/parser/argv/shortOptsNoSpace.test.js
@@ -9,3 +9,13 @@ test('shortOptsNoSpace README example works', () => {
 
   expect(argv).toStrictEqual(exp)
 })
+
+test('shortOptsNoSpace does not touch options without single minus sign', () => {
+  const obj = {argv: ['abc', '--nLogan']}
+
+  const {argv} = shortOptsNoSpace(obj)
+
+  const exp = ['abc', '--nLogan']
+
+  expect(argv).toStrictEqual(exp)
+})
