0ad0137736b3ac62707dbc85d688ca060db7538b
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-06 16:58:57 +0200

    test: test if optsList uses default style if style is undefined

--- src/help/usage/optsList.test.js
+++ src/help/usage/optsList.test.js
@@ -172,3 +172,27 @@ test('optsList collects args from the same key', () => {
 
   expect(res).toStrictEqual(txt)
 })
+
+test('optsList uses default style if style is undefined', () => {
+  const opts = [
+    {key: 'answer', types: ['number'], args: ['-a', '--answer'], desc: 'The answer.', required: true},
+    {key: 'foo', types: ['string'], args: ['-f', '--foo'], desc: 'Foo.', only: ['foo', 'bar'], required: false},
+    {key: 'baz', types: ['bool'], args: ['-b', '--baz'], desc: 'Baz.', descArg: 'baz', defaultValues: [42], implies: ['foo']},
+    {key: 'help', types: [], args: ['--help', 'help', '-h'], desc: 'Prints help.', defaultValues: [false]},
+    {key: 'version', types: [], args: ['--version'], desc: 'Prints version.', contradicts: ['help']},
+    {key: 'question', types: ['string'], required: true, desc: 'The question.'},
+    {key: 'politePhrase', types: null, variadic: true, desc: 'Polite phrases.'}
+  ]
+
+  const res = optsList(opts)()
+
+  const txt = '-a, --answer=<number>    The answer. [required]                                 \n' +
+              '-f, --foo=<foo|bar>      Foo. [not required]                                    \n' +
+              '-b, --baz=<baz>          Baz. [default: 42] [implies: -f, --foo]                \n' +
+              '-h, help, --help         Prints help. [default: false]                          \n' +
+              '--version                Prints version. [contradicts: --help, help, -h]        \n' +
+              '<question>               The question. [required]                               \n' +
+              '<politePhrase>...        Polite phrases.                                        \n'
+
+  expect(res).toStrictEqual(txt)
+})
