09f7f11949398cecb47b025c98f3aa65e52193b0
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-06 16:56:41 +0200

    test: test if optsList works as expected

--- src/help/usage/optsList.test.js
+++ src/help/usage/optsList.test.js
@@ -22,3 +22,37 @@ test('optsList README example works as expected', () => {
   
   expect(res).toStrictEqual(txt)
 })
+
+test('optsList works as expected', () => {
+  const opts = [
+    {key: 'answer', types: null, args: ['answer'], desc: 'The answer.', required: true},
+    {key: 'foo', types: ['string'], args: ['-f', '--foo'], desc: 'Foo.', only: ['foo', 'bar'], required: false},
+    {key: 'baz', types: ['bool'], args: ['-b', '--baz'], desc: 'Baz.', descArg: 'baz', defaultValues: [42], implies: ['foo']},
+    {key: 'help', types: [], args: ['--help', 'help', '-h'], desc: 'Prints help.', defaultValues: [false]},
+    {key: 'version', types: [], args: ['--version'], desc: 'Prints version.', contradicts: ['help']},
+    {key: 'position', types: ['number', 'number'], args: ['--pos'], desc: 'The position.'},
+    {key: 'question', types: ['string'], required: true, desc: 'The question.'},
+    {key: 'politePhrase', types: null, variadic: true, desc: 'Polite phrases.'},
+    {now: 'something', completely: 'different'}
+  ]
+
+  const style = {
+    cols: [
+      {width: 25},
+      {width: 50}
+    ]
+  }
+
+  const res = optsList(opts)(style)
+
+  const txt = 'answer                   The answer. [required]                            \n' +
+              '-f, --foo=<foo|bar>      Foo. [not required]                               \n' +
+              '-b, --baz=<baz>          Baz. [default: 42] [implies: -f, --foo]           \n' +
+              '-h, help, --help         Prints help. [default: false]                     \n' +
+              '--version                Prints version. [contradicts: --help, help, -h]   \n' +
+              '--pos=<number number>    The position.                                     \n' +
+              '<question>               The question. [required]                          \n' +
+              '<politePhrase>...        Polite phrases.                                   \n'
+
+  expect(res).toStrictEqual(txt)
+})
