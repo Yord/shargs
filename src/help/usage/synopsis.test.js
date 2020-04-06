5f81641db185d9079b1aa07f03ff50151047bbad
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-06 15:51:34 +0200

    test: test if synopsisFrom with wrong id uses default style

--- src/help/usage/synopsis.test.js
+++ src/help/usage/synopsis.test.js
@@ -237,3 +237,28 @@ test('synopsisFrom correctly passes on id', () => {
 
   expect(res).toStrictEqual(txt)
 })
+
+test('synopsisFrom with wrong id uses default style', () => {
+  const opts = [
+    {key: 'answer', types: ['number'], args: ['-a', '--answer'], required: true},
+    {key: 'help', types: [], args: ['-h', '--help']},
+    {key: 'verbose', types: [], args: ['-v']},
+    {key: 'verbose', types: [], args: ['-q'], reverse: true},
+    {values: 'yay'},
+    {key: 'fun', types: ['bool'], args: ['-f'], required: true},
+    {key: 'fun', types: ['bool'], args: ['--no-fun'], reverse: true},
+    {key: 'question', types: ['string'], required: true},
+    {key: 'politePhrase', types: null, variadic: true}
+  ]
+
+  const style = {
+    line: {width: 40}
+  }
+
+  const res = synopsisFrom('custom')('deepThought')(opts)(style)
+
+  const txt = 'deepThought (-a|--answer) [-h|--help] [-v|-q] [--no-fun] (-f) (<question>)      \n' +
+              '            [<politePhrase>...]                                                 \n'
+
+  expect(res).toStrictEqual(txt)
+})
