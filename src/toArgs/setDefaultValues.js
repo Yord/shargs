5bd601d789c2845989ea3513166383bca9827012
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-14 15:03:59 +0200

    feat: allow default values for flags

--- src/toArgs/setDefaultValues.js
+++ src/toArgs/setDefaultValues.js
@@ -9,17 +9,21 @@ module.exports = ({errs = [], opts: OPTS = []} = {}) => {
     const {key, types, values, defaultValues} = opt
 
     if (typeof values === 'undefined' && typeof defaultValues !== 'undefined') {
-      if (Array.isArray(types)) {
-        if (Array.isArray(defaultValues) && defaultValues.length === types.length) {
-          args[key] = defaultValues.length === 1 ? defaultValues[0] : defaultValues
-        } else {
-          errs2.push(invalidDefaultValues({defaultValues, option: opt}))
-        }
-      } else {
+      if (!Array.isArray(types)) {
         args[key] = defaultValues
+      } else if (types.length === 0 && typeof defaultValues === 'object' && isFlag(defaultValues)) {
+        args[key] = defaultValues
+      } else if (Array.isArray(defaultValues) && types.length === defaultValues.length) {
+        args[key] = defaultValues.length === 1 ? defaultValues[0] : defaultValues
+      } else {
+        errs2.push(invalidDefaultValues({defaultValues, option: opt}))
       }
     }
   }
 
   return {errs: errs.concat(errs2), args}
+}
+
+function isFlag ({type, count}) {
+  return type === 'flag' && typeof count === 'number'
 }
