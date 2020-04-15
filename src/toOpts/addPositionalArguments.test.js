5e035b1052e57de87e5cacad581ed40cc8ea75fb
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-15 12:07:55 +0200

    test: test if addPositionalArguments works with variadic pos args

--- src/toOpts/addPositionalArguments.test.js
+++ src/toOpts/addPositionalArguments.test.js
@@ -21,3 +21,28 @@ test('addPositionalArguments works as expected', () => {
 
   expect(opts3).toStrictEqual(exp)
 })
+
+test('addPositionalArguments works with variadic pos args', () => {
+  const answer = {key: 'answer', types: ['number'], args: ['-a', '--answer'], values: ['42']}
+  const pos1   = {key: 'pos1', types: ['number'], args: null}
+  const pos2   = {key: 'pos2', types: null, args: null, variadic: true}
+
+  const opts = [answer, pos1, pos2]
+
+  const opts2 = [
+    answer,
+    {values: ['23']},
+    {values: ['19']},
+    {values: ['42']}
+  ]
+
+  const {opts: opts3} = addPositionalArguments(opts)({opts: opts2})
+
+  const exp = [
+    answer,
+    {...pos1, values: ['23']},
+    {...pos2, types: ['string', 'string'], values: ['19', '42']},
+  ]
+
+  expect(opts3).toStrictEqual(exp)
+})
