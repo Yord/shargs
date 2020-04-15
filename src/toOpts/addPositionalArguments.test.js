c976495764bb440373cf1f3b37e092c517862baf
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-15 12:08:16 +0200

    test: test if addPositionalArguments works with variadic pos args that eat up all values if they are not defined last

--- src/toOpts/addPositionalArguments.test.js
+++ src/toOpts/addPositionalArguments.test.js
@@ -46,3 +46,27 @@ test('addPositionalArguments works with variadic pos args', () => {
 
   expect(opts3).toStrictEqual(exp)
 })
+
+test('addPositionalArguments works with variadic pos args that eat up all values if they are not defined last', () => {
+  const answer = {key: 'answer', types: ['number'], args: ['-a', '--answer'], values: ['42']}
+  const pos1   = {key: 'pos1', types: ['number'], args: null}
+  const pos2   = {key: 'pos2', types: null, args: null, variadic: true}
+
+  const opts = [answer, pos2, pos1]
+
+  const opts2 = [
+    answer,
+    {values: ['23']},
+    {values: ['19']},
+    {values: ['42']}
+  ]
+
+  const {opts: opts3} = addPositionalArguments(opts)({opts: opts2})
+
+  const exp = [
+    answer,
+    {...pos2, types: ['string', 'string', 'string'], values: ['23', '19', '42']},
+  ]
+
+  expect(opts3).toStrictEqual(exp)
+})
