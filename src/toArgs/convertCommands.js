8f617bd6fc85a43c14e1780d73bc377e9ae3b024
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-15 11:39:04 +0200

    chore: do not check for positional arguments in toArgs any more

--- src/toArgs/convertCommands.js
+++ src/toArgs/convertCommands.js
@@ -1,11 +1,9 @@
-const {invalidRequiredPositionalArgument, invalidVariadicPositionalArgument, requiredPositionalArgumentMissing} = require('../errors')
-
 module.exports = parsers => ({errs = [], opts: OPTS = []} = {}) => {
   let args  = {_: []}
   let errs2 = []
 
   for (let i = 0; i < OPTS.length; i++) {
-    const {key, values, types, opts, posArgs} = OPTS[i]
+    const {key, values, types, opts} = OPTS[i]
 
     if (Array.isArray(values) && types === null) {
       const parentParser = parsers.__
@@ -15,31 +13,6 @@ module.exports = parsers => ({errs = [], opts: OPTS = []} = {}) => {
         args[key] = {}
 
         const child = childParser(opts || [])(values, [])
-
-        if (Array.isArray(posArgs)) {
-          const errs3 = validatePositionalArguments(posArgs)
-          errs2       = errs2.concat(errs3)
-
-          for (let j = 0; j < posArgs.length; j++) {
-            const posArg = posArgs[j]
-            const {key: key2, types: types2, required = false, variadic = false} = posArg
-
-            const values2 = child.args._.slice(0, variadic === true ? child.args._.length : types2.length)
-
-            if (types2 === null && required === true && values2.length === 0) {
-              errs2.push(requiredPositionalArgumentMissing({key: key2, positionalArgument: posArg}))
-            } else if (types2 === null || values2.length === types2.length) {
-               if (typeof args[key][key2] === 'undefined') {
-                 args[key][key2] = values2.length === 1 ? values2[0] : values2
-               }
-
-               child.args._ = child.args._.slice(types2 === null ? child.args._.length : types2.length)
-            } else {
-              errs2.push(requiredPositionalArgumentMissing({key: key2, positionalArgument: posArg}))
-            }
-          }
-        }
-
         errs2       = errs2.concat(child.errs || [])
         args[key]   = Object.assign({}, args[key], child.args)
 
@@ -69,38 +42,4 @@ function isNotRest ({types}) {
 
 function hasNoValues ({values}) {
   return typeof values === 'undefined'
-}
-
-function validatePositionalArguments (posArgs) {
-  const errs = []
-
-  const invalidRequired = posArgs.reduce(
-    (bool, {required = false}) => {
-      if (required === false || required === true) {
-        if (bool === null) {
-          return required ? null : false
-        } else {
-          return bool || required
-        }
-      } else {
-        return true
-      }
-    },
-    null
-  )
-
-  if (invalidRequired === true) {
-    errs.push(invalidRequiredPositionalArgument({positionalArguments: posArgs}))
-  }
-
-  const invalidVariadic = posArgs.slice(0, posArgs.length - 1).reduce(
-    (bool, {variadic = false}) => bool || variadic,
-    false
-  )
-
-  if (invalidVariadic === true) {
-    errs.push(invalidVariadicPositionalArgument({positionalArguments: posArgs}))
-  }
-
-  return errs
 }
