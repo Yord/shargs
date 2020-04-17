ab8ca695e2a45b22e5b065f6f947c30fc9dff083
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-17 23:05:37 +0200

    refactor: adjust pairArgvWithArgs to changed option types

--- src/toOpts/pairArgvWithArgs.js
+++ src/toOpts/pairArgvWithArgs.js
@@ -7,26 +7,27 @@ module.exports = (opts = []) => {
   return ({errs = [], argv: ARGV = []} = {}) => {
     const errs2 = errs.concat(ERRS)
     const opts2 = []
-  
+
     let at  = 0
     let ARG = ARGV[at]
-  
+
     while (ARG) {
       const options = args[ARG]
-  
+
       let at2 = at + 1
-  
+
       if (Array.isArray(options)) {
         for (let j = 0; j < options.length; j++) {
           const option = options[j]
-  
+
           let {types} = option
           let values  = []
-          if (typeof types === 'undefined' || types === null) {
+          if (typeof types === 'undefined') {
+
             let i = at + 1
             let arg = ARGV[i] || '--'
             while (arg !== '--') {
-              if (types === null) types = []
+              if (typeof types === 'undefined') types = []
               types.push('string')
               values.push(arg)
               i++
@@ -37,20 +38,20 @@ module.exports = (opts = []) => {
           } else {
             values = ARGV.slice(at + 1, at + types.length + 1)
           }
-  
+
           opts2.push({...option, values})
-  
-          at2 = at + (types === null ? 0 : types.length) + 1
+
+          at2 = at + (typeof types === 'undefined' ? 0 : types.length) + 1
         }
       } else {
         const values = ARGV.slice(at, at + 1)
         opts2.push({values})
       }
-  
+
       at  = at2
       ARG = ARGV[at]
     }
-  
+
     return {errs: errs2, opts: opts2}
   }
 }
