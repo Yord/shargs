dc68e4faf82f52366690060d46475a238c856c64
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-27 10:20:47 +0200

    refactor: make error codes camel case

--- src/errors.js
+++ src/errors.js
@@ -1,29 +1,29 @@
 const illegalKeyName = ({key, option}) => ({
-  code: 'Illegal key name',
+  code: 'IllegalKeyName',
   msg:  'An option key had an illegal name.',
   info: {key, option}
 })
 
 const invalidDefaultValues = ({defaultValues, option}) => ({
-  code: 'Invalid default values',
+  code: 'InvalidDefaultValues',
   msg:  "An option's defaultValues field has an invalid type. It must be an array with any values in it.",
   info: {defaultValues, option}
 })
 
 const invalidOptionsListInCombine = ({options, arg, argument}) => ({
-  code: 'Invalid options list in combine',
+  code: 'InvalidOptionsListInCombine',
   msg:  'Options list in combine was undefined, null or empty',
   info: {options, arg, argument}
 })
 
 const invalidTypes = ({types, option}) => ({
-  code: 'Invalid types',
+  code: 'InvalidTypes',
   msg:  'Each argument must have a types key that must be null or an array',
   info: {types, option}
 })
 
 const nonMatchingArgumentTypes = ({arg, ref, option}) => ({
-  code: 'Non-matching argument types',
+  code: 'NonMatchingArgumentTypes',
   msg:  'If arguments have the same arg, their types must either be equal or have the same length',
   info: {arg, ref, option}
 })
