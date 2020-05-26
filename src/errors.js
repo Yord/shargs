f14c2ba80134d8fecd9532953201e53899f2995b
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-26 11:28:51 +0200

    chore: add sensible error messages

--- src/errors.js
+++ src/errors.js
@@ -1,60 +1,60 @@
 const CommandExpected = ({opt}) => ({
   code: 'CommandExpected',
-  msg:  '', // with key and opts array
+  msg:  'Expected a command with a string "key" field and an "opts" array.',
   info: {opt}
 })
 
 const InvalidArgs = ({opt}) => ({
   code: 'InvalidArgs',
-  msg:  '', // TODO: args syntax is wrong
+  msg:  'The "args" field has an invalid value: "args" must be a non-empty array of strings.',
   info: {opt}
 })
 
 const InvalidKey = ({opt}) => ({
   code: 'InvalidKey',
-  msg:  '', // TODO: args syntax is wrong
+  msg:  'The "key" field has an invalid value: "key" must be a string and cannot be "_" or "--".',
   info: {opt}
 })
 
 const InvalidNestedCommand = ({opt}) => ({
   code: 'InvalidNestedCommand',
-  msg:  '', // TODO: error because commands cannot be wrapped in commands
+  msg:  'Commands cannot be nested inside commands. Did you forget an "args" field for your subcommand?',
   info: {opt}
 })
 
 const InvalidOpts = ({opt}) => ({
   code: 'InvalidOpts',
-  msg:  '', // TODO: opts syntax is wrong
+  msg:  'The "opts" field has an invalid value: "opts" must be an array of command-line options and positional arguments.',
   info: {opt}
 })
 
 const InvalidTypes = ({opt}) => ({
   code: 'InvalidTypes',
-  msg:  '', // TODO: types syntax is wrong
+  msg:  'The "types" field has an invalid value: "types" must be an array of strings.',
   info: {opt}
 })
 
 const OptionExpected = ({opt}) => ({
   code: 'OptionExpected',
-  msg:  '',
+  msg:  'A command-line option was expected, but something else was received.',
   info: {opt}
 })
 
 const PosArgExpected = ({opt}) => ({
   code: 'PosArgExpected',
-  msg:  '',
+  msg:  'A positional argument was expected, but something else was received.',
   info: {opt}
 })
 
 const SubcommandExpected = ({opt}) => ({
   code: 'SubcommandExpected',
-  msg:  '',
+  msg:  'A subcommand was expected, but something else was received.',
   info: {opt}
 })
 
 const UnknownCommandLineOptionType = ({opt}) => ({
   code: 'UnknownCommandLineOptionType',
-  msg:  '', // TODO: unknown option type error
+  msg:  'The command-line option or positional argument given is of an unknown type.',
   info: {opt}
 })
 
