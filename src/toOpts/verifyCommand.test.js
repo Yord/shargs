5888bf3b16f699b1b6c572e9acca49f45f193d8b
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-26 16:53:18 +0200

    test: test if verifyCommand fails for opts with invalid args

--- src/toOpts/verifyCommand.test.js
+++ src/toOpts/verifyCommand.test.js
@@ -162,3 +162,23 @@ test('verifyCommand fails for opts with invalid key', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('verifyCommand fails for opts with invalid args', () => {
+  const foo = {key: 'arc', args: null, types: []}
+
+  const opt = {
+    key: 'foo',
+    opts: [
+      foo
+    ]
+  }
+
+  const res = verifyCommand(opt)
+
+  const exp = {
+    errs: [OptionExpected({opt: foo}), InvalidArgs({opt: foo})],
+    opt: {...opt, opts: []}
+  }
+
+  expect(res).toStrictEqual(exp)
+})
