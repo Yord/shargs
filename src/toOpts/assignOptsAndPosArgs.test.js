d54c4a50b2fbe086c60ce6952a7a10e363c000c0
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-26 16:57:56 +0200

    test: test if assignOptsAndPosArgs works for variadic options 1/3

--- src/toOpts/assignOptsAndPosArgs.test.js
+++ src/toOpts/assignOptsAndPosArgs.test.js
@@ -264,3 +264,28 @@ test('assignOptsAndPosArgs works for duplicate array options', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('assignOptsAndPosArgs works for variadic options 1/3', () => {
+  const foo = {key: 'foo', args: ['-f']}
+
+  const opt = {
+    opts: [
+      foo
+    ]
+  }
+
+  const errs = []
+
+  const argv = ['-f', 'baz', 'bat']
+
+  const res = assignOptsAndPosArgs(opt)({errs, argv})
+
+  const exp = {
+    errs: [],
+    opts: [
+      {...foo, types: ['string', 'string'], values: ['baz', 'bat']}
+    ]
+  }
+
+  expect(res).toStrictEqual(exp)
+})
