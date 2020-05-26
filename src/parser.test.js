ee688e3acf3d8c6d7e479ab67cd1c93af6ee6d12
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-26 11:33:51 +0200

    test: test if parser works with duplicate primitive options by taking the first option

--- src/parser.test.js
+++ src/parser.test.js
@@ -382,3 +382,34 @@ test('parser works with primitive options', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('parser works with duplicate primitive options by taking the first option', () => {
+  const stages = {}
+
+  const parsers = {}
+
+  const arc = {key: 'arc', args: ['-a'], types: ['A']}
+
+  const opt = {
+    key: 'Foo',
+    opts: [
+      arc
+    ]
+  }
+
+  const argv = ['-a', '1', '-a', '2']
+
+  const errs = []
+
+  const res = parser(stages, parsers)(opt)(argv, errs)
+
+  const exp = {
+    errs: [],
+    args: {
+      _: [],
+      arc: '1'
+    }
+  }
+
+  expect(res).toStrictEqual(exp)
+})
