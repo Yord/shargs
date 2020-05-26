5b7583b837e374b8957fef75488595bc3a529178
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-26 11:38:17 +0200

    test: test if parser works with duplicate subcommands by setting fromArgs to the identity function

--- src/parser.test.js
+++ src/parser.test.js
@@ -961,3 +961,55 @@ test('parser works with duplicate subcommands by only taking the first', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('parser works with duplicate subcommands by setting fromArgs to the identity function', () => {
+  const identity = a => a
+
+  const stages = {
+    fromArgs: identity
+  }
+
+  const parsers = {}
+
+  const arc = {key: 'arc', args: ['-a'], types: []}
+  const Arc = {key: 'Arc', args: ['Arc'], opts: [
+    arc
+  ]}
+
+  const opt = {
+    key: 'Foo',
+    opts: [
+      Arc,
+      arc
+    ]
+  }
+
+  const argv = ['-a', '-a', 'Arc', '-a', 'Arc', '1']
+
+  const errs = []
+
+  const res = parser(stages, parsers)(opt)(argv, errs)
+
+  const exp = {
+    errs: [],
+    args: [
+      {
+        _: [],
+        arc: {type: 'flag', count: 2}
+      },
+      {
+        Arc: {
+          _: [],
+          arc: {type: 'flag', count: 1}
+        }
+      },
+      {
+        Arc: {
+          _: ['1']
+        }
+      }
+    ]
+  }
+
+  expect(res).toStrictEqual(exp)
+})
