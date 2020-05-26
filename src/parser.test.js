4430c3e1ff1e1956ec109dbc1a1897918893d05b
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-26 11:32:23 +0200

    test: test if parser works with undefined stages

--- src/parser.test.js
+++ src/parser.test.js
@@ -148,3 +148,42 @@ test('parser works with undefined errs', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('parser works with undefined stages', () => {
+  const stages = {
+    toArgv:   undefined,
+    argv:     undefined,
+    toOpts:   undefined,
+    opts:     undefined,
+    toArgs:   undefined,
+    args:     undefined,
+    fromArgs: undefined
+  }
+
+  const parsers = {}
+
+  const arc = {key: 'arc', args: ['-a'], types: ['A']}
+
+  const opt = {
+    key: 'Foo',
+    opts: [
+      arc
+    ]
+  }
+
+  const argv = ['-a', '1']
+
+  const errs = []
+
+  const res = parser(stages, parsers)(opt)(argv, errs)
+
+  const exp = {
+    errs: [],
+    args: {
+      _: [],
+      arc: '1'
+    }
+  }
+
+  expect(res).toStrictEqual(exp)
+})
