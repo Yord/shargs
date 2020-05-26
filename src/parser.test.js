c9f162f67a72a939ff367bf4cd8068179aca5c98
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-26 11:36:10 +0200

    test: test if parser works with variadic positional arguments

--- src/parser.test.js
+++ src/parser.test.js
@@ -692,3 +692,34 @@ test('parser works with duplicate variadic options with -- by taking only the fi
 
   expect(res).toStrictEqual(exp)
 })
+
+test('parser works with variadic positional arguments', () => {
+  const stages = {}
+
+  const parsers = {}
+
+  const arc = {key: 'arc'}
+
+  const opt = {
+    key: 'Foo',
+    opts: [
+      arc
+    ]
+  }
+
+  const argv = ['1', '2', '3']
+
+  const errs = []
+
+  const res = parser(stages, parsers)(opt)(argv, errs)
+
+  const exp = {
+    errs: [],
+    args: {
+      _: [],
+      arc: ['1', '2', '3']
+    }
+  }
+
+  expect(res).toStrictEqual(exp)
+})
