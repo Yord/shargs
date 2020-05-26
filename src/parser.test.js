cb318bddb62ad307707cd0887b096ab123d272dc
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-26 11:32:36 +0200

    test: test if parser works with sample stages

--- src/parser.test.js
+++ src/parser.test.js
@@ -187,3 +187,39 @@ test('parser works with undefined stages', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('parser works with sample stages', () => {
+  const stages = {
+    toArgv:         ({errs, any }) => ({errs, argv: any }),
+    argv:           [a             => a                  ],
+    toOpts:   () => ({errs, argv}) => ({errs, opts: argv}),
+    opts:           [a             => a                  ],
+    toArgs:         ({errs, opts}) => ({errs, args: opts}),
+    args:           [a             => a                  ],
+    fromArgs:       a              => a
+  }
+
+  const parsers = {}
+
+  const arc = {key: 'arc', args: ['-a'], types: ['A']}
+
+  const opt = {
+    key: 'Foo',
+    opts: [
+      arc
+    ]
+  }
+
+  const argv = ['-a', '1']
+
+  const errs = []
+
+  const res = parser(stages, parsers)(opt)(argv, errs)
+
+  const exp = {
+    errs: [],
+    args: ['-a', '1']
+  }
+
+  expect(res).toStrictEqual(exp)
+})
