99de9ce31cebb051cb6a6188d59a23fcc403dd1d
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-27 11:03:22 +0200

    test: test if toOpts works for nonempty command and empty argv

--- src/toOpts/index.test.js
+++ src/toOpts/index.test.js
@@ -20,3 +20,29 @@ test('toOpts works for empty command and empty argv', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('toOpts works for nonempty command and empty argv', () => {
+  const arc = {key: 'arc'}
+
+  const opt = {
+    key: 'opt',
+    opts: [
+      arc
+    ]
+  }
+
+  const errs = []
+
+  const argv = []
+
+  const res = toOpts(opt)({errs, argv})
+
+  const exp = {
+    errs: [],
+    opts: [
+      arc
+    ]
+  }
+
+  expect(res).toStrictEqual(exp)
+})
