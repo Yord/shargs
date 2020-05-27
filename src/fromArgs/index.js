e711605a408fbbb7b1ea2f16d5cf848ab4886372
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-27 12:09:45 +0200

    fix: fix merging subcommands of subcommands in fromArgs

--- src/fromArgs/index.js
+++ src/fromArgs/index.js
@@ -18,9 +18,27 @@ function merge (args) {
         args2[key] = [...(args2[key] || []), ...value]
       } else if (typeof args2[key] === 'undefined') {
         args2[key] = value
+      } else if (isObject(value)) {
+        args2[key] = mergeDeep(args2[key], value)
       }
     }
   }
 
   return args2
+}
+
+function mergeDeep (obj1, obj2) {
+  for (let [key, value] of Object.entries(obj2)) {
+    if (isObject(value)) {
+      obj1[key] = mergeDeep(obj1[key] || {}, value)
+    } else if (typeof obj1[key] === 'undefined') {
+      obj1[key] = value
+    }
+  }
+
+  return obj1
+}
+
+function isObject (a) {
+  return !Array.isArray(a) && a !== null && typeof a === 'object'
 }
