9f3e0b712dae3c9739456a4f71d15645f55f08b2
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-27 11:10:45 +0200

    test: test if toArgs ignores options and positional arguments without values

--- src/toArgs/index.test.js
+++ src/toArgs/index.test.js
@@ -63,3 +63,36 @@ test('toArgs works for two rest values', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('toArgs ignores options and positional arguments without values', () => {
+  const arc = {key: 'arc', args: ['-a'], types: []}
+  const bar = {key: 'bar', args: ['-b'], types: ['A']}
+  const cat = {key: 'cat', args: ['-c'], types: ['B', 'C']}
+  const dot = {key: 'dot', types: ['D']}
+  const eat = {key: 'eat', types: ['E', 'F']}
+  const Arc = {key: 'Arc', args: ['Arc'], opts: []}
+
+  const errs = []
+
+  const opts = [
+    arc,
+    bar,
+    cat,
+    dot,
+    eat,
+    Arc
+  ]
+
+  const res = toArgs({errs, opts})
+
+  const exp = {
+    errs: [],
+    args: [
+      {
+        _: []
+      }
+    ]
+  }
+
+  expect(res).toStrictEqual(exp)
+})
