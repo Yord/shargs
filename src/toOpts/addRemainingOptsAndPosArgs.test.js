f37da4cd74c306fd1f8b06fee76186af2765c2c3
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-27 11:00:03 +0200

    test: test if addRemainingOptsAndPosArgs works for flag options

--- src/toOpts/addRemainingOptsAndPosArgs.test.js
+++ src/toOpts/addRemainingOptsAndPosArgs.test.js
@@ -22,3 +22,28 @@ test('addRemainingOptsAndPosArgs works for rest values', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('addRemainingOptsAndPosArgs works for flag options', () => {
+  const foo = {key: 'foo', types: [], args: ['-f']}
+
+  const opt = {
+    opts: [
+      foo
+    ]
+  }
+
+  const errs = []
+
+  const opts = []
+
+  const res = addRemainingOptsAndPosArgs(opt)({errs, opts})
+
+  const exp = {
+    errs: [],
+    opts: [
+      foo
+    ]
+  }
+
+  expect(res).toStrictEqual(exp)
+})
