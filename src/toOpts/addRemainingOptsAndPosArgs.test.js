ee5b873f590029b75bb63deefeb709afaaf529ab
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-27 11:00:56 +0200

    test: test if addRemainingOptsAndPosArgs works for variadic options

--- src/toOpts/addRemainingOptsAndPosArgs.test.js
+++ src/toOpts/addRemainingOptsAndPosArgs.test.js
@@ -97,3 +97,28 @@ test('addRemainingOptsAndPosArgs works for array options', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('addRemainingOptsAndPosArgs works for variadic options', () => {
+  const foo = {key: 'foo', args: ['-f']}
+
+  const opt = {
+    opts: [
+      foo
+    ]
+  }
+
+  const errs = []
+
+  const opts = []
+
+  const res = addRemainingOptsAndPosArgs(opt)({errs, opts})
+
+  const exp = {
+    errs: [],
+    opts: [
+      foo
+    ]
+  }
+
+  expect(res).toStrictEqual(exp)
+})
