6d76cbad5c30122e9227235dea001ffc332757c5
Philipp Wille <philipp.y.wille@gmail.com>
2020-05-27 11:00:31 +0200

    test: test if addRemainingOptsAndPosArgs works for primitive options

--- src/toOpts/addRemainingOptsAndPosArgs.test.js
+++ src/toOpts/addRemainingOptsAndPosArgs.test.js
@@ -47,3 +47,28 @@ test('addRemainingOptsAndPosArgs works for flag options', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('addRemainingOptsAndPosArgs works for primitive options', () => {
+  const foo = {key: 'foo', types: ['foo'], args: ['-f']}
+
+  const opt = {
+    opts: [
+      foo
+    ]
+  }
+
+  const errs = []
+
+  const opts = []
+
+  const res = addRemainingOptsAndPosArgs(opt)({errs, opts})
+
+  const exp = {
+    errs: [],
+    opts: [
+      foo
+    ]
+  }
+
+  expect(res).toStrictEqual(exp)
+})
