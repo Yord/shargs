c51171461693ed9a273da87c04b92897f3b133f2
Philipp Wille <philipp.y.wille@gmail.com>
2020-06-02 10:23:24 +0200

    test: test if parser works with duplicate array positional arguments by adding all remaining to the rest array

--- src/parserSync.test.js
+++ src/parserSync.test.js
@@ -1171,6 +1171,37 @@ test('parserSync works with duplicate array positional arguments by adding all r
   expect(res).toStrictEqual(exp)
 })
 
+test('parser works with duplicate array positional arguments by adding all remaining to the rest array', async () => {
+  const stages = {}
+
+  const substages = {}
+
+  const arc = {key: 'arc', types: ['A', 'B']}
+
+  const opt = {
+    key: 'Foo',
+    opts: [
+      arc
+    ]
+  }
+
+  const argv = ['1', '2', '3', '4']
+
+  const errs = []
+
+  const res = await parser(stages, substages)(opt)(argv, errs)
+
+  const exp = {
+    errs: [],
+    args: {
+      _: ['3', '4'],
+      arc: ['1', '2']
+    }
+  }
+
+  expect(res).toStrictEqual(exp)
+})
+
 test('parserSync works with variadic options', () => {
   const stages = {}
 
