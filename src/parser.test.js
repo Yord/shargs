b71dc9b49c59aff7ba545e1c3dbf28037d5c4581
Philipp Wille <philipp.y.wille@gmail.com>
2020-06-03 12:03:54 +0200

    test: test if parser works with default substages 4

--- src/parser.test.js
+++ src/parser.test.js
@@ -2571,6 +2571,72 @@ test('parserSync works with default substages 4', () => {
   expect(res).toStrictEqual(exp)
 })
 
+test('parser works with default substages 4', async () => {
+  const stages = {}
+
+  const substages = {
+    _: {
+      _: [
+        ({errs, opts}) => ({
+          errs,
+          opts: opts.map(opt =>
+            ({...opt, ...(opt.key === 'arc' ? {values: ['foo']} : {})})
+          )
+        })
+      ]
+    }
+  }
+
+  const arc = {key: 'arc', args: ['-a'], types: ['A']}
+  const Cat = {key: 'Cat', args: ['Cat'], opts: [
+    arc
+  ]}
+  const Bar = {key: 'Bar', args: ['Bar'], opts: [
+    arc
+  ]}
+  const Arc = {key: 'Arc', args: ['Arc'], opts: [
+    arc,
+    Bar,
+    Cat
+  ]}
+
+  const opt = {
+    key: 'Foo',
+    opts: [
+      Arc,
+      arc
+    ]
+  }
+
+  const argv = ['-a', '1', 'Arc', '-a', '2', 'Bar', '-a', '3', 'Cat', '-a', '4']
+
+  const errs = []
+
+  const res = await parser(stages, substages)(opt)(argv, errs)
+
+  const exp = {
+    errs: [],
+    args: {
+      _: [],
+      arc: '1',
+      Arc: {
+        _: [],
+        arc: '2',
+        Bar: {
+          _: [],
+          arc: 'foo'
+        },
+        Cat: {
+          _: [],
+          arc: 'foo'
+        }
+      }
+    }
+  }
+
+  expect(res).toStrictEqual(exp)
+})
+
 test('parserSync works with duplicate subcommands by only taking the first', () => {
   const stages = {}
 
