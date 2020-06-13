5df2d7d196b1bab664af1a41ec63b2f216eaf000
Philipp Wille <philipp.y.wille@gmail.com>
2020-06-13 14:07:27 +0200

    refactor: factor out a lexer and Sync and Async modes from parser

--- src/parser.js
+++ src/parser.js
@@ -1,95 +1,29 @@
+const {Async}               = require('./Async')
 const {fromArgs: FROM_ARGS} = require('./fromArgs')
-const {pipe}                = require('./pipe')
-const {then}                = require('./then')
+const {lexerF}              = require('./lexer')
 const {toArgs:   TO_ARGS}   = require('./toArgs')
-const {toArgv:   TO_ARGV}   = require('./toArgv')
-const {toOpts:   TO_OPTS}   = require('./toOpts')
-
-const Sync = {
-  resolve: a => a,
-  then:    pipe,
-  all:     a => a
-}
-
-const Async = {
-  resolve: a => Promise.resolve(a),
-  then:    then,
-  all:     a => Promise.all(a)
-}
+const {Sync}                = require('./Sync')
 
 module.exports = {
-  parser:     parser(Async),
-  parserSync: parser(Sync)
+  parserF,
+  parser:     parserF(Async),
+  parserSync: parserF(Sync)
 }
 
-function parser (Mode) {
+function parserF (Mode) {
   return (stages = {}, substages = {}) => {
     const {
-      toArgv   = TO_ARGV,
-      argv     = [],
-      toOpts   = TO_OPTS,
-      opts     = [],
       toArgs   = TO_ARGS,
       args     = [],
       fromArgs = FROM_ARGS
     } = stages
   
     return opt => Mode.then(
-      toArgv,
-      ...argv,
-      toOpts(opt),
-      recurseOpts(Mode)(opts, substages),
+      lexerF(Mode)(stages, substages)(opt),
       toArgs,
       transformArgs(Mode)(args),
       fromArgs
     )
-  }
-}
-
-function recurseOpts (Mode) {
-  return (optsStages, substages, optsStages2) => ({errs = [], opts = []} = {errs: [], opts: []}) => {
-    const stages = optsStages2 || optsStages
-
-    const promise1 = Mode.then(...stages)({errs, opts})
-    
-    return Mode.then(result => {
-      const {errs: errs3 = [], opts: opts3 = []} = result || {errs, opts: []}
-
-      const promises = opts3.map(opt => {
-        if (isSubcommand(opt) && hasValues(opt)) {
-          const stages = (
-            Array.isArray(substages[opt.key]) ? substages[opt.key] :
-            Array.isArray(substages._)        ? substages._
-                                              : undefined
-          )
-          const substages2 = substages[opt.key] || substages._ || {}
-  
-          const promise2 = recurseOpts(Mode)(optsStages, substages2, stages)({errs: [], opts: opt.values})
-          
-          return Mode.then(({errs: errs4, opts: opts4}) => {
-            return {
-              errs: errs4,
-              opts: [{...opt, values: opts4}]
-            }
-          })(promise2)
-        } else {
-          return Mode.resolve({
-            errs: [],
-            opts: [opt]
-          })
-        }
-      })
-
-      return Mode.then(results => {
-        return results.reduce(
-          ({errs, opts}, {errs: errs2, opts: opts2}) => ({
-            errs: [...errs, ...errs2],
-            opts: [...opts, ...opts2],
-          }),
-          {errs: errs3, opts: []}
-        )
-      })(Mode.all(promises))
-    })(promise1)
   }
 }
 
@@ -109,17 +43,4 @@ function transformArgs (Mode) {
       )
     })(Mode.all(promises))
   }
-}
-
-function isSubcommand ({key, args, types, opts}) {
-  return (
-    typeof key === 'string' &&
-    Array.isArray(args) && args.length > 0 &&
-    typeof types === 'undefined' &&
-    Array.isArray(opts)
-  )
-}
-
-function hasValues ({values}) {
-  return Array.isArray(values)
 }
